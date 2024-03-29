6c04e4fee719469a685149f80650804e690c3554
audgns10 <s23065@gsm.hs.kr>
2024-03-29 19:19:40 +0900

    :sparkles: :: buildSrc module 추가 및 관련 파일 추가

---
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -0,0 +1,62 @@
+object Dependency {
+    object Gradle {
+        const val APPLICATION = "com.android.application"
+        const val LIBRARY = "com.android.library"
+        const val KOTLIN = "org.jetbrains.kotlin.android"
+        const val KSP = "com.google.devtools.ksp"
+    }
+
+    object AndroidX {
+        const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+        const val LIFECYCLE = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.LIFECYCLE}"
+        const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
+    }
+
+    object Compose {
+        const val ACTIVITY_COMPOSE = "androidx.activity:activity-compose:${Versions.ACTIVITY_COMPOSE}"
+        const val COMPOSE = "androidx.compose.ui:ui:${Versions.COMPOSE}"
+        const val COMPOSE_PREVIEW = "androidx.compose.ui:ui-tooling-preview:${Versions.COMPOSE}"
+        const val COMPOSE_MATERIAL3 = "androidx.compose.material3:material3:${Versions.COMPOSE_MATERIAL3}"
+        const val COMPOSE_MATERIAL = "androidx.compose.material:material:${Versions.COMPOSE_MATERIAL}"
+        const val COMPOSE_TOOLING = "androidx.compose.ui:ui-tooling:${Versions.COMPOSE}"
+        const val COMPOSE_GRAPHICS = "androidx.compose.ui:ui-graphics:${Versions.COMPOSE}"
+        const val COMPOSE_BOM = "androidx.compose:compose-bom:${Versions.COMPOSE_BOM}"
+        const val COMPOSE_NAVIGATION = "androidx.navigation:navigation-compose:${Versions.COMPOSE_NAVIGATION}"
+        const val COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:{${Versions.COMPOSE_FOUNDATION}"
+    }
+
+    object Google {
+        const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+
+        const val GOOGLE_SERVICES_PLUGIN = "com.google.gms.google-services"
+        // FIREBASE
+    }
+
+    object Coil {
+        const val COIL = "io.coil-kt:coil-compose:${Versions.COIL}"
+    }
+
+    object Retrofit {
+        const val RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}"
+        const val RETROFIT_CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}"
+    }
+
+    object OkHttp {
+        const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
+        const val OKHTTP_LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:${Versions.OKHTTP}"
+    }
+
+    object Hilt {
+        const val HILT_PLUGIN = "com.google.dagger.hilt.android"
+        const val HILT = "com.google.dagger:hilt-android:${Versions.HILT}"
+        const val HILT_COMPILER = "com.google.dagger:hilt-android-compiler:${Versions.HILT}"
+    }
+
+    object Test {
+        const val JUNIT = "junit:junit:${Versions.JUNIT}"
+        const val ANDROID_JUNIT = "androidx.test.ext:junit:${Versions.ANDROID_JUNIT}"
+        const val ESPRESSO = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
+        const val COMPOSE_JUNIT = "androidx.compose.ui:ui-test-junit4:${Versions.COMPOSE}"
+        const val COMPOSE_MANIFEST = "androidx.compose.ui:ui-test-manifest:${Versions.COMPOSE}"
+    }
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,33 @@
+object Versions {
+
+    const val GRADLE = "8.2.0"
+    const val KOTLIN = "1.8.10"
+    const val KSP = "1.8.10-1.0.9"
+
+    const val COMPOSE = "1.6.2"
+    const val APPCOMPAT = "1.6.1"
+    const val ACTIVITY_COMPOSE = "1.7.2"
+    const val COMPOSE_MATERIAL = "1.4.3"
+    const val COMPOSE_MATERIAL3 = "1.1.1"
+    const val COMPOSE_BOM = "2023.03.00"
+    const val COMPOSE_NAVIGATION = "2.7.6"
+    const val COMPOSE_FOUNDATION = "1.6.1"
+
+    const val LIFECYCLE = "2.3.1"
+    const val CORE_KTX = "1.5.0"
+    const val MATERIAL = "1.9.0"
+    const val VIEWMODEL_LIFECYCLE = "2.5.1"
+
+    const val JUNIT = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.2"
+    const val ESPRESSO = "3.5.1"
+
+    const val RETROFIT = "2.7.1"
+    const val OKHTTP = "4.10.0"
+
+    const val COIL = "2.4.0"
+
+    const val HILT = "2.47"
+
+    const val GOOGLE_SERVICE_PLUGIN = "4.3.15"
+}
