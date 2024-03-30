74b30751bc2c98bc3c99453f0854bb208c911f73
Github <s23065@gsm.hs.kr>
2024-03-30 19:32:17 +0900

    :bug: :: material -> material3

--- presentation/src/main/java/com/chobo/presentation/view/theme/Theme.kt
+++ presentation/src/main/java/com/chobo/presentation/view/theme/Theme.kt
@@ -1,6 +1,6 @@
 package com.chobo.presentation.view.theme
 
-import androidx.compose.material.Typography
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import com.chobo.presentation.view.theme.color.ColorTheme
 import com.chobo.presentation.view.theme.color.MindWayColor
