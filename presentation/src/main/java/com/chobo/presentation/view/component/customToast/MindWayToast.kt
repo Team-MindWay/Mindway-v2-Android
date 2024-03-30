45944315748c81dae4fafdd9d74a8affeb41d69d
youloveli <mymhj122@gmail.com>
2024-03-30 14:25:26 +0900

    :lipstick: :: MindWayToast 퍼블리싱

---
+++ presentation/src/main/java/com/chobo/presentation/view/component/customToast/MindWayToast.kt
@@ -0,0 +1,56 @@
+package com.chobo.presentation.view.component.customToast
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chobo.presentation.R
+import com.chobo.presentation.view.component.icon.FailIcon
+import com.chobo.presentation.view.component.icon.SuccessIcon
+import com.chobo.presentation.view.foundation.MindWayColor
+
+@Composable
+fun MindWayToast(
+    modifier: Modifier = Modifier,
+    text: String,
+    isSuccess: Boolean,
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .shadow(
+                elevation = 20.dp,
+                spotColor = MindWayColor.StatusShadow,
+                ambientColor = MindWayColor.StatusShadow
+            )
+            .background(color = MindWayColor.WHITE, shape = RoundedCornerShape(size = 8.dp))
+            .padding(start = 16.dp, top = 20.dp, end = 16.dp, bottom = 20.dp)
+    ) {
+        if (isSuccess) SuccessIcon()
+        else FailIcon()
+        Text(
+            text = text,
+
+            style = TextStyle(
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard)),
+                fontWeight = FontWeight(400),
+                color = MindWayColor.Black,
+            )
+        )
+    }
+}
