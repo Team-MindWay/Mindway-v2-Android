493456ce536c8147099c5a5240610d4a8f8ce420
Github <s23065@gsm.hs.kr>
2024-03-30 19:23:32 +0900

    :lipstick: :: add ColorTheme, MindWayAndroidTheme and modify Color

---
+++ presentation/src/main/java/com/chobo/presentation/view/theme/color/ColorTheme.kt
@@ -0,0 +1,24 @@
+package com.chobo.presentation.view.theme.color
+
+import androidx.compose.ui.graphics.Color
+
+abstract class ColorTheme {
+    abstract val MAIN: Color
+    abstract val Black: Color
+    abstract val WHITE: Color
+
+    abstract val GRAY900: Color
+    abstract val GRAY800: Color
+    abstract val GRAY700: Color
+    abstract val GRAY600: Color
+    abstract val GRAY500: Color
+    abstract val GRAY400: Color
+    abstract val GRAY300: Color
+    abstract val GRAY200: Color
+    abstract val GRAY100: Color
+
+    abstract val SYSTEM: Color
+
+    abstract val CardShadow: Color
+    abstract val StatusShadow: Color
+}
