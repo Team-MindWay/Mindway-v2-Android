fbabac7b81588a483a51b8b983e46b8a0c1e5dbe
youloveli <mymhj122@gmail.com>
2024-03-30 16:15:22 +0900

    :lipstick: :: MindWayTextField 퍼블리싱

---
+++ presentation/src/main/java/com/chobo/presentation/view/component/textField/MindWayTextField.kt
@@ -0,0 +1,90 @@
+package com.chobo.presentation.view.component.textField
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chobo.presentation.R
+import com.chobo.presentation.view.foundation.MindWayColor
+
+@Composable
+fun MindWayTextField(
+    modifier: Modifier = Modifier,
+    title: String,
+    textState: MutableState<String>,
+    hint: String,
+    isError: Boolean,
+    errorMessage: String,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+
+        ) {
+        Text(
+            text = title,
+
+            style = TextStyle(
+                fontSize = 14.sp,
+                lineHeight = 21.sp,
+                fontFamily = FontFamily(Font(R.font.pretendard)),
+                fontWeight = FontWeight(400),
+                color = MindWayColor.GRAY400,
+            )
+        )
+        BasicTextField(
+            onValueChange = { newText ->
+                textState.value = newText
+            },
+            value = textState.value,
+            modifier = modifier
+                .border(
+                    width = 1.dp,
+                    color = if (isError) MindWayColor.SYSTEM
+                    else MindWayColor.GRAY100,
+                    shape = RoundedCornerShape(size = 8.dp)
+                )
+                .fillMaxSize()
+                .background(color = MindWayColor.GRAY100, shape = RoundedCornerShape(size = 8.dp))
+                .padding(start = 15.dp, top = 15.dp, end = 15.dp, bottom = 15.dp),
+            decorationBox = {
+                if (textState.value == "") {
+                    Text(
+                        text = hint,
+                        style = TextStyle(color = MindWayColor.GRAY500),
+                        modifier = Modifier.padding(start = 15.dp, top = 15.dp)
+                    )
+                }
+            },
+        )
+
+        if (isError) {
+            Text(
+                text = errorMessage,
+
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 21.sp,
+                    fontFamily = FontFamily(Font(R.font.pretendard)),
+                    fontWeight = FontWeight(400),
+                    color = MindWayColor.SYSTEM
+                ),
+            )
+        }
+    }
+}
