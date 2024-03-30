83249b6500a7d19e1d02de68cd4fb494cfb50aaa
Github <s23065@gsm.hs.kr>
2024-03-30 23:46:23 +0900

    :bug: :: Modify Kotlin, Compose Versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
 
     const val GRADLE = "8.2.0"
-    const val KOTLIN = "1.8.10"
+    const val KOTLIN = "1.7.20"
     const val KSP = "1.8.10-1.0.9"
 
-    const val COMPOSE = "1.6.2"
+    const val COMPOSE = "1.4.3"
     const val APPCOMPAT = "1.6.1"
     const val ACTIVITY_COMPOSE = "1.7.2"
     const val COMPOSE_MATERIAL = "1.4.3"
