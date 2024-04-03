68cd9d41db870532c5103294c88db2d89dcfe59e
Github <s23065@gsm.hs.kr>
2024-04-03 23:02:10 +0900

    :sparkles: :: MainActivity startDestination LoginScreen으로 설정

--- app/src/main/java/com/chobo/mindway_v2_android/MainActivity.kt
+++ app/src/main/java/com/chobo/mindway_v2_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.chobo.mindway_v2_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.chobo.mindway_v2_android.ui.theme.Mindwayv2AndroidTheme
 import com.chobo.presentation.view.login.navigation.NAVIGATION_LOGIN
@@ -11,6 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             Mindwayv2AndroidTheme {
