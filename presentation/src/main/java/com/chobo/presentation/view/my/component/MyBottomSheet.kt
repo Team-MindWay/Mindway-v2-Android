dc6b732c0667a4b13d8930713b0c968faddfe63d
혜성 <118062596+answad@users.noreply.github.com>
2024-04-06 11:03:39 +0900

    :white_check_mark: :: MyBottomSheet 퍼블리싱

---
+++ presentation/src/main/java/com/chobo/presentation/view/my/component/MyBottomSheet.kt
@@ -0,0 +1,61 @@
+package com.chobo.presentation.view.my.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chobo.presentation.R
+import com.chobo.presentation.view.theme.MindWayAndroidTheme
+
+@Composable
+fun MyBottomSheet() {
+    MindWayAndroidTheme { colors, typography ->
+        Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = colors.WHITE,
+                    shape = RoundedCornerShape(size = 12.dp)
+                )
+                .padding(start = 24.dp, top = 28.dp, end = 24.dp, bottom = 48.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.mindway_intro),
+                style = typography.bodySmall,
+                fontWeight = FontWeight.Normal,
+                color = colors.Black,
+            )
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(color = colors.GRAY100)
+            )
+            Text(
+                text = "로그아웃",
+                style = typography.bodySmall,
+                fontWeight = FontWeight.Normal,
+                color = colors.SYSTEM,
+            )
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun MyBottomSheetPreview() {
+    MyBottomSheet()
+}
