03dffe868ab41a65e8e75fe9e650c88886e6e55c
명훈 <s23065@gsm.hs.kr>
2024-04-08 07:57:08 +0900

    :fire: :: 불필요한 import 제거

--- presentation/src/main/java/com/chobo/presentation/view/book/screen/CheckOutBookScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/book/screen/CheckOutBookScreen.kt
@@ -1,23 +1,15 @@
 package com.chobo.presentation.view.book.screen
 
-import android.view.Window
 import androidx.compose.foundation.background
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -25,10 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.chobo.presentation.view.component.textField.MindWayTextField
-import com.chobo.presentation.view.theme.MindWayAndroidTheme
 import com.chobo.presentation.R
 import com.chobo.presentation.view.component.button.MindWayButton
+import com.chobo.presentation.view.component.textField.MindWayTextField
+import com.chobo.presentation.view.theme.MindWayAndroidTheme
 
 @Composable
 fun CheckOutBookScreen(
