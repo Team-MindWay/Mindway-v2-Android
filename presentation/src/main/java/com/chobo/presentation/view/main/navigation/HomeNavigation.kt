737df7b481bc2f81265fb1aac70f6dbb2b641e30
혜성 <118062596+answad@users.noreply.github.com>
2024-04-07 23:22:30 +0900

    :recycle: :: MockData추가

--- presentation/src/main/java/com/chobo/presentation/view/main/navigation/HomeNavigation.kt
+++ presentation/src/main/java/com/chobo/presentation/view/main/navigation/HomeNavigation.kt
@@ -3,7 +3,10 @@ package com.chobo.presentation.view.main.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.chobo.presentation.view.main.component.BookKingOfTheMonthData
+import com.chobo.presentation.view.main.component.ReadingGoalGraphData
 import com.chobo.presentation.view.main.screen.HomeScreen
+import com.chobo.presentation.view.main.screen.MockOnClick
 
 const val homeRoute = "home_route"
 
@@ -13,6 +16,23 @@ fun NavController.navigationToHome() {
 
 fun NavGraphBuilder.homeScreen() {
     composable(homeRoute) {
-        HomeScreen()
+        HomeScreen(
+            homeNoticeCardOnClick = { MockOnClick() },
+            homeGoalReadingChart = { MockOnClick() },
+            readingGoalGraphDataList = listOf(
+                ReadingGoalGraphData(2, 3, false, "일"),
+                ReadingGoalGraphData(3, 3, false, "일"),
+                ReadingGoalGraphData(2, 3, false, "일"),
+                ReadingGoalGraphData(1, 3, true, "일"),
+                ReadingGoalGraphData(2, 3, false, "일"),
+                ReadingGoalGraphData(1, 3, false, "일"),
+                ReadingGoalGraphData(2, 3, false, "일"),
+            ),
+            bookKingOfTheMonthDataList = listOf(
+                BookKingOfTheMonthData("왕승황", 29),
+                BookKingOfTheMonthData("왕성찬", 15),
+                BookKingOfTheMonthData("왕지완", 1),
+            )
+        )
     }
 }
