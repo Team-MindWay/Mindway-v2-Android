f783f59ee5f4ba82c597d05830a7065d40be7a38
yeongun130 <yeongunj625@gmail.com>
2024-04-08 20:49:55 +0900

    :sparkles: :: add event navigation

--- app/src/main/java/com/chobo/mindway_v2_android/MindWayNavHost.kt
+++ app/src/main/java/com/chobo/mindway_v2_android/MindWayNavHost.kt
@@ -4,6 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.chobo.presentation.view.event.navigation.detailEventScreen
+import com.chobo.presentation.view.event.navigation.eventScreen
+import com.chobo.presentation.view.event.navigation.navigationToDetailEvent
+import com.chobo.presentation.view.event.navigation.navigationToEvent
 import com.chobo.presentation.view.login.navigation.loginScreen
 import com.chobo.presentation.view.main.navigation.homeScreen
 import com.chobo.presentation.view.main.navigation.navigationToHome
@@ -23,5 +27,15 @@ fun MindWayNavHost(
             navigateToHome = { navController.navigationToHome() }
         )
         homeScreen()
+        eventScreen(
+            navigateToHome = { navController.navigationToHome() },
+            navigateToEvent = { navController.navigationToEvent() },
+            navigateToBooks = {  },
+            navigateToMy = { },
+            navigateToDetailEvent = { navController.navigationToDetailEvent() }
+        )
+        detailEventScreen(
+            navigateToEvent = { navController.navigationToEvent() }
+        )
     }
 }
