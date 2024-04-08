dd49d579bbf08b6f6fb2da123536d74eb628c203
혜성 <118062596+answad@users.noreply.github.com>
2024-04-08 23:45:30 +0900

    :sparkles: :: viewModel추가

--- presentation/src/main/java/com/chobo/presentation/view/event/screen/EventScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/screen/EventScreen.kt
@@ -1,36 +1,22 @@
 package com.chobo.presentation.view.event.screen
 
-import android.text.Layout
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.R
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBar
-import com.chobo.presentation.view.component.icon.BookIcon
-import com.chobo.presentation.view.component.icon.BookImage
+import com.chobo.presentation.view.event.component.EventContent
 import com.chobo.presentation.view.event.component.EventPager
-import com.chobo.presentation.view.event.component.Events
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
+import com.chobo.presentation.viewModel.EventViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -39,7 +25,8 @@ fun EventScreen(
     navigateToHome: () -> Unit,
     navigateToEvent: () -> Unit,
     navigateToBooks: () -> Unit,
-    navigateToMy: () -> Unit
+    navigateToMy: () -> Unit,
+    eventViewModel: EventViewModel = viewModel()
 ) {
     val tabs = listOf(
         stringResource(id = R.string.ongoing_event),
@@ -50,15 +37,27 @@ fun EventScreen(
     }
     MindWayAndroidTheme { colors, _ ->
         Box(
-            modifier= modifier
+            modifier = modifier
                 .fillMaxSize()
                 .background(color = colors.WHITE)
-        ){
+        ) {
             EventPager(
                 pagerState = pagerState,
                 tabs = tabs,
-                onGoingEvent = { OngoingEvent() },
-                pastEvent = { PastEvent() }
+                onGoingEvent = {
+                    EventContent(
+                        content = stringResource(R.string.is_no_ongoing_event),
+                        eventDataList = eventViewModel.currentEventsDataList,
+                        onIconClick = eventViewModel::onCurrentIconClick
+                    )
+                },
+                pastEvent = {
+                    EventContent(
+                        content = stringResource(R.string.is_no_past_event),
+                        eventDataList = eventViewModel.pastEventsDataList,
+                        onIconClick = eventViewModel::onPastIconClick
+                    )
+                }
             )
             MindWayNavBar(
                 modifier = modifier.align(Alignment.BottomCenter),
@@ -66,73 +65,6 @@ fun EventScreen(
                 navigateToEvent = { navigateToEvent() },
                 navigateToBooks = { navigateToBooks() },
                 navigateToMy = { navigateToMy() }
-            )
-        }
-    }
-}
-
-@Composable
-fun OngoingEvent(
-    modifier: Modifier = Modifier
-) {
-    MindWayAndroidTheme { colors, _ ->
-        LazyColumn(
-            modifier = modifier
-                .background(color = colors.WHITE)
-                .padding(horizontal = 24.dp)
-        ) {
-            items(10) {
-                Events(
-                    title = "가을 독서 행사",
-                    content = "독서의 계절, 가을을 맞아 도서관에서 특별한 이벤트를준비했습니다. 랜덤으로 초성 책 제목이 적혀있는 쪽지를 뽑고, 그에 맞는",
-                    date = "2024년 04월 08일"
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun PastEvent(
-    modifier: Modifier = Modifier
-) {
-    MindWayAndroidTheme { colors, _ ->
-        LazyColumn(
-            modifier = modifier
-                .background(color = colors.WHITE)
-                .padding(horizontal = 24.dp)
-        ) {
-            items(10) {
-                Events(
-                    title = "가을 독서 행사",
-                    content = "독서의 계절, 가을을 맞아 도서관에서 특별한 이벤트를준비했습니다. 랜덤으로 초성 책 제목이 적혀있는 쪽지를 뽑고, 그에 맞는",
-                    date = "2024년 04월 08일"
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun NotEvent(
-    modifier: Modifier = Modifier,
-    content: String
-) {
-    MindWayAndroidTheme { colors, typography ->
-        Column(
-            modifier = modifier
-                .fillMaxSize()
-                .background(colors.WHITE),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            BookImage()
-            Spacer(modifier = modifier.height(20.dp))
-            Text(
-                text = content,
-                style = typography.bodyMedium,
-                color = colors.GRAY500,
-                fontWeight = FontWeight.Normal
             )
         }
     }
