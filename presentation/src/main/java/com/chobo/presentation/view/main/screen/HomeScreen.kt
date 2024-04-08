4e3fa6aa69fb71d599245bcbd56191de7cd73006
명훈 <s23065@gsm.hs.kr>
2024-04-08 13:40:34 +0900

    :lipstick: :: add_bottomnavigationbar

--- presentation/src/main/java/com/chobo/presentation/view/main/screen/HomeScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/main/screen/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.chobo.presentation.view.main.screen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBar
 import com.chobo.presentation.view.main.component.HomeGoalReadingChart
 import com.chobo.presentation.view.main.component.HomeNoticeCard
 import com.chobo.presentation.view.main.component.HomeReadersOfTheMonthChart
@@ -20,30 +22,39 @@ import com.chobo.presentation.viewModel.HomeViewModel
 fun HomeScreen(
     homeViewModel: HomeViewModel
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = 24.dp)
-    ) {
-        HomeNoticeCard(
-            titleText = "가을 독서 행사",
-            content = "독서의 계절, 가을을 맞아 \n도서관에서 특별한 이벤트를 준비했습니다.",
-            onClick = { homeViewModel.homeNoticeCardOnClick() }
-        )
-        HomeGoalReadingChart(
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.CenterVertically),
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .fillMaxWidth()
-                .height(211.dp),
-            isHasData = true,
-            readNumberList = homeViewModel.readingGoalGraphDataList,
-            onClick = { homeViewModel.homeGoalReadingChartOnClick(1) }
-        )
-        HomeReadersOfTheMonthChart(
-            isHasData = true,
-            bookKingOfTheMonthData = homeViewModel.bookKingOfTheMonthDataList
-        )
+                .fillMaxSize()
+                .padding(horizontal = 24.dp)
+        ) {
+            HomeNoticeCard(
+                titleText = "가을 독서 행사",
+                content = "독서의 계절, 가을을 맞아 \n도서관에서 특별한 이벤트를 준비했습니다.",
+                onClick = { homeViewModel.homeNoticeCardOnClick() }
+            )
+            HomeGoalReadingChart(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(211.dp),
+                isHasData = true,
+                readNumberList = homeViewModel.readingGoalGraphDataList,
+                onClick = { homeViewModel.homeGoalReadingChartOnClick(1) }
+            )
+            HomeReadersOfTheMonthChart(
+                isHasData = true,
+                bookKingOfTheMonthData = homeViewModel.bookKingOfTheMonthDataList
+            )
+        }
+        MindWayNavBar(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            navigateToHome = { /*TODO*/ },
+            navigateToEvent = { /*TODO*/ },
+            navigateToBooks = { /*TODO*/ }
+        ) {
+        }
     }
 }
 
