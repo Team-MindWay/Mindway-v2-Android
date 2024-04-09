9746b47475afeb8d838524352a37d8524db44ba8
혜성 <118062596+answad@users.noreply.github.com>
2024-04-09 13:56:02 +0900

    :sparkles: :: navigation 추가

---
+++ presentation/src/main/java/com/chobo/presentation/view/book/navigation/BookNavigation.kt
@@ -0,0 +1,31 @@
+package com.chobo.presentation.view.book.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.chobo.presentation.view.book.screen.BookAddBookScreen
+import com.chobo.presentation.view.book.screen.BookScreen
+
+const val BookAddBookRoute = "book_add_book_route"
+const val BookRoute = "book_route"
+
+fun NavController.navigationToAddBook() {
+    this.navigate(BookAddBookRoute)
+}
+
+fun NavController.navigationToBook() {
+    this.navigate(BookRoute)
+}
+
+
+fun NavGraphBuilder.bookAddBook() {
+    composable(BookAddBookRoute) {
+        BookAddBookScreen()
+    }
+}
+
+fun NavGraphBuilder.book() {
+    composable(BookRoute) {
+        BookScreen()
+    }
+}
