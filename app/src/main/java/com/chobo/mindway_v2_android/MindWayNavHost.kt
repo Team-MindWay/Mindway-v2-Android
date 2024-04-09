c4f6d3deab955259586bf29e62a626e869e16250
혜성 <118062596+answad@users.noreply.github.com>
2024-04-09 13:56:26 +0900

    :sparkles: :: NavHost에 각 화면 등록

--- app/src/main/java/com/chobo/mindway_v2_android/MindWayNavHost.kt
+++ app/src/main/java/com/chobo/mindway_v2_android/MindWayNavHost.kt
@@ -4,13 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.chobo.presentation.view.book.navigation.book
+import com.chobo.presentation.view.book.navigation.bookAddBook
 import com.chobo.presentation.view.event.navigation.detailEventScreen
 import com.chobo.presentation.view.event.navigation.eventScreen
 import com.chobo.presentation.view.event.navigation.navigationToDetailEvent
 import com.chobo.presentation.view.event.navigation.navigationToEvent
 import com.chobo.presentation.view.login.navigation.loginScreen
+import com.chobo.presentation.view.main.navigation.addBook
+import com.chobo.presentation.view.main.navigation.goalReading
 import com.chobo.presentation.view.main.navigation.homeScreen
 import com.chobo.presentation.view.main.navigation.navigationToHome
+import com.chobo.presentation.view.main.navigation.viewDetail
+import com.chobo.presentation.view.my.navigation.introScreen
+import com.chobo.presentation.view.my.navigation.myBookEditScreen
+import com.chobo.presentation.view.my.navigation.myScreen
 
 @Composable
 fun MindWayNavHost(
@@ -27,6 +35,9 @@ fun MindWayNavHost(
             navigateToHome = { navController.navigationToHome() }
         )
         homeScreen()
+        goalReading()
+        viewDetail()
+        addBook()
         eventScreen(
             navigateToHome = { navController.navigationToHome() },
             navigateToEvent = { navController.navigationToEvent() },
@@ -37,5 +48,10 @@ fun MindWayNavHost(
         detailEventScreen(
             navigateToEvent = { navController.navigationToEvent() }
         )
+        bookAddBook()
+        book()
+        introScreen()
+        myScreen()
+        myBookEditScreen()
     }
 }
