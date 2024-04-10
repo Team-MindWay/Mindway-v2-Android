dbb563e587abe08e7d469f5b1dcdcd14f20a84c4
명훈 <s23065@gsm.hs.kr>
2024-04-11 08:32:18 +0900

    :recycle:

--- presentation/src/main/java/com/chobo/presentation/view/event/component/DetailEventTopBar.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/component/DetailEventTopBar.kt
@@ -7,13 +7,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.chobo.presentation.R
 import com.chobo.presentation.view.component.icon.ChevronLeftIcon
-import com.chobo.presentation.view.component.topBar.MindWayOnlyStartTopAppBar
+import com.chobo.presentation.view.component.topBar.MindWayTopAppBar
 
 @Composable
 fun DetailEventTopBar(
     startIconClick: () -> Unit
 ){
-    MindWayOnlyStartTopAppBar(
+    MindWayTopAppBar(
         startIcon = { ChevronLeftIcon(modifier = Modifier.clickable(interactionSource = MutableInteractionSource(), indication = null) { startIconClick() }) },
         midText = stringResource(id = R.string.ongoing_event)
     )
