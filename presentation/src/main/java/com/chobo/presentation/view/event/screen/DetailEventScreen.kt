db05bbb937f12c501a9038ff56e426b36210650f
명훈 <s23065@gsm.hs.kr>
2024-04-11 00:53:05 +0900

    :recycle: :: DetailEventTopBar

--- presentation/src/main/java/com/chobo/presentation/view/event/screen/DetailEventScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/screen/DetailEventScreen.kt
@@ -13,11 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.chobo.presentation.R
 import com.chobo.presentation.view.event.component.DetailEventContent
 import com.chobo.presentation.view.event.component.DetailEventTopBar
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
@@ -36,10 +34,9 @@ fun DetailEventScreen(
                 .background(color = colors.WHITE)
                 .padding(horizontal = 24.dp)
         ) {
-            DetailEventTopBar(
-                title = stringResource(id = R.string.ongoing_event),
-                navigateToEvent = navigateToEvent
-            )
+            DetailEventTopBar {
+
+            }
             Spacer(modifier = modifier.height(20.dp))
             Image(
                 painter = painterResource(detailEventViewModel.returnImageResId()),
