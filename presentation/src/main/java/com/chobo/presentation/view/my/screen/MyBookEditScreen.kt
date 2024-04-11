2c94dab05a435db84ac91e5f93a0828ba112cfb7
명훈 <s23065@gsm.hs.kr>
2024-04-11 11:47:04 +0900

    ♻️ :: MyBookEditScreen에 button을 추가합니다.

--- presentation/src/main/java/com/chobo/presentation/view/my/screen/MyBookEditScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/my/screen/MyBookEditScreen.kt
@@ -2,7 +2,11 @@ package com.chobo.presentation.view.my.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -14,7 +18,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.R
+import com.chobo.presentation.view.component.button.MindWayButton
 import com.chobo.presentation.view.component.textField.MindWayTextField
+import com.chobo.presentation.view.theme.MindWayAndroidTheme
 import com.chobo.presentation.viewModel.MyBookEditViewModel
 
 @Composable
@@ -24,7 +30,7 @@ fun MyBookEditScreen(
     val titleState = remember { mutableStateOf(myBookEditViewModel.titleTextState.value) }
     val writerState = remember { mutableStateOf(myBookEditViewModel.writeTextState.value) }
     val linkState = remember { mutableStateOf(myBookEditViewModel.linkTextState.value) }
-
+MindWayAndroidTheme { colors, _ ->
     Column(
         verticalArrangement = Arrangement.spacedBy(28.dp, Alignment.Top),
         horizontalAlignment = Alignment.Start,
@@ -34,6 +40,7 @@ fun MyBookEditScreen(
                 horizontal = 24.dp,
                 vertical = 28.dp
             )
+            .imePadding()
     ) {
         MindWayTextField(
             title = stringResource(R.string.title),
@@ -53,7 +60,16 @@ fun MyBookEditScreen(
             placeholder = stringResource(R.string.please_enter_the_link),
             errorMessage = stringResource(R.string.please_enter_the_link)
         )
+        Spacer(modifier = Modifier.weight(1f))
+        MindWayButton(
+            text = stringResource(id = R.string.apply),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            onClick = { myBookEditViewModel.checkButton() }
+        )
     }
+}
 }
 @Preview(showBackground = true)
 @Composable
