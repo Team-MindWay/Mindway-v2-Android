dff04f0a55dc3472d927dc096d07d7262522b364
혜성 <118062596+answad@users.noreply.github.com>
2024-04-12 11:03:00 +0900

    :recycle: :: 네비게이션의 시작을 홈으로 변경

--- presentation/src/main/java/com/chobo/presentation/view/component/bottom_navigation_bar/MindWayNavBar.kt
+++ presentation/src/main/java/com/chobo/presentation/view/component/bottom_navigation_bar/MindWayNavBar.kt
@@ -9,13 +9,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
+
 @Composable
 fun MindWayNavBar(
     modifier: Modifier = Modifier,
@@ -24,7 +25,7 @@ fun MindWayNavBar(
     navigateToBooks: () -> Unit,
     navigateToMy: () -> Unit
 ) {
-    var navigationIndex by remember { mutableStateOf(-1) }
+    var navigationIndex by remember { mutableIntStateOf(0) }
     val itemList = listOf(
         MindWayNavBarItemType.HOME,
         MindWayNavBarItemType.EVENT,
