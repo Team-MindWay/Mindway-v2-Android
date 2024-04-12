fa10a44bad5da918eabc39f639a5fa020eeda56f
혜성 <118062596+answad@users.noreply.github.com>
2024-04-12 20:07:40 +0900

    :lipstick: :: navigation에 myBookEditScreen 추가

--- presentation/src/main/java/com/chobo/presentation/view/my/navigation/MyNavigation.kt
+++ presentation/src/main/java/com/chobo/presentation/view/my/navigation/MyNavigation.kt
@@ -4,16 +4,28 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.chobo.presentation.view.my.screen.MindWayIntroScreen
+import com.chobo.presentation.view.my.screen.MyBookEditScreen
 
 const val introRoute = "intro_route"
+const val myBookEditRoute = "my_book_edit_route"
 
 
 fun NavController.navigationToIntro() {
     this.navigate(introRoute)
 }
+fun NavController.navigationToMyBookEdit() {
+    this.navigate(myBookEditRoute)
+}
+
 
 fun NavGraphBuilder.introScreen(navigateToBack: () -> Unit) {
     composable(introRoute) {
         MindWayIntroScreen(navigateToBack = navigateToBack)
+    }
+}
+
+fun NavGraphBuilder.myBookEditScreen(navigateToBack: () -> Unit) {
+    composable(myBookEditRoute) {
+        MyBookEditScreen(navigateToBack = navigateToBack)
     }
 }
