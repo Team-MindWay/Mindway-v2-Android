d72614cc4beb7221b06b5a3f57e4bc81a2702d9f
명훈 <s23065@gsm.hs.kr>
2024-04-14 13:43:36 +0900

    :sparkles: :: BookAddBookScreen에 Dialog 구현

--- presentation/src/main/java/com/chobo/presentation/view/book/screen/BookAddBookScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/book/screen/BookAddBookScreen.kt
@@ -10,15 +10,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.R
+import com.chobo.presentation.view.book.component.BookPopUp
 import com.chobo.presentation.view.book.component.BookRequestTopAppBar
 import com.chobo.presentation.view.component.button.MindWayButton
 import com.chobo.presentation.view.component.textField.MindWayTextField
@@ -34,6 +38,7 @@ fun BookAddBookScreen(
     val titleTextState = remember { mutableStateOf(bookAddBookViewModel.titleTextState.value) }
     val writeTextState = remember { mutableStateOf(bookAddBookViewModel.writeTextState.value) }
     val linkTextState = remember { mutableStateOf(bookAddBookViewModel.linkTextState.value) }
+    var checkBookDialog by remember { mutableStateOf(true) }
 
     MindWayAndroidTheme { colors, _ ->
         Column (modifier = modifier.background(color = colors.WHITE)){
@@ -60,6 +65,13 @@ fun BookAddBookScreen(
                             vertical = 28.dp
                         )
                 ) {
+                    if (checkBookDialog) {
+                        Dialog(onDismissRequest = { checkBookDialog = false }) {
+                            BookPopUp(
+                                onDismiss = { checkBookDialog = false }
+                            )
+                        }
+                    }
                     MindWayTextField(
                         title = stringResource(id = R.string.title),
                         textState = titleTextState,
