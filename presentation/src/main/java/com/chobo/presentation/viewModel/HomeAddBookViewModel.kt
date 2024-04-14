ee8a75bd14c58f63101b0188de7f097beb6dd364
혜성 <118062596+answad@users.noreply.github.com>
2024-04-14 12:32:13 +0900

    :recycle: :: stateflow를 보내는방법을 asStateFlow()로 변경

--- presentation/src/main/java/com/chobo/presentation/viewModel/HomeAddBookViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/HomeAddBookViewModel.kt
@@ -4,19 +4,18 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeAddBookViewModel @Inject constructor() : ViewModel() {
     private val _titleTextState = MutableStateFlow("")
-    val titleTextState: StateFlow<String>
-        get() = _titleTextState
+    val titleTextState: StateFlow<String> = _titleTextState.asStateFlow()
     val titleTextMaxLength
         get() = 60
 
     private val _contentTextState = MutableStateFlow("")
-    val contentTextState: StateFlow<String>
-        get() = _contentTextState
+    val contentTextState: StateFlow<String> = _contentTextState.asStateFlow()
     val contentTextMaxLength
         get() = 300
     fun checkButton() {
