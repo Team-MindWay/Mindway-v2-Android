9952ad7141b77c51a5d039ac2af228208438d16c
혜성 <118062596+answad@users.noreply.github.com>
2024-04-14 13:25:12 +0900

    :recycle: :: stateFlow적용

--- presentation/src/main/java/com/chobo/presentation/view/main/screen/GoalReadingScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/main/screen/GoalReadingScreen.kt
@@ -10,15 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.view.main.component.GoalReadingChart
-import com.chobo.presentation.view.main.component.GoalReadingGraphData
 import com.chobo.presentation.view.main.component.GoalReadingListOfBooksReadItem
 import com.chobo.presentation.view.main.component.GoalReadingPlusCard
 import com.chobo.presentation.view.main.component.GoalReadingTopAppBar
@@ -33,13 +34,17 @@ fun GoalReadingScreen(
     navigateToHomeAddBook: () -> Unit,
     navigateToHomeViewDetail: () -> Unit
 ) {
+    val goalBookRead by goalReadingViewModel.goalBookRead.collectAsState()
+    val goalReadingGraphDataList by goalReadingViewModel.goalReadingGraphDataList.collectAsState()
+    val goalReadingListOfBooksReadItemDataList by goalReadingViewModel.goalReadingListOfBooksReadItemDataList.collectAsState()
+
     MindWayAndroidTheme { colors, _ ->
         Column(modifier = modifier.background(color = colors.WHITE)) {
             Spacer(modifier = Modifier.height(20.dp))
             GoalReadingTopAppBar(
                 startIconOnClick = { navigateToBack() },
                 endIconOnClick = { },
-                isData = goalReadingViewModel.goalBookRead.collectAsState().value == 0
+                isData = goalBookRead == 0
             )
             Column(
                 verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
@@ -67,14 +72,14 @@ fun GoalReadingScreen(
                                 modifier = Modifier
                                     .fillMaxWidth()
                                     .height(180.dp),
-                                goalBookRead = goalReadingViewModel.goalBookRead.collectAsState().value,
+                                goalBookRead = goalBookRead,
                                 goalReadingGraphData = goalReadingGraphDataList
                             )
                         }
                         item {
                             GoalReadingPlusCard(onClick = navigateToHomeAddBook)
                         }
-                        items(goalReadingViewModel.goalReadingListOfBooksReadItemDataList) { item ->
+                        items(goalReadingListOfBooksReadItemDataList) { item ->
                             GoalReadingListOfBooksReadItem(
                                 data = item,
                                 onClick = navigateToHomeViewDetail
--- presentation/src/main/java/com/chobo/presentation/viewModel/GoalReadingViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/GoalReadingViewModel.kt
@@ -2,20 +2,17 @@ package com.chobo.presentation.viewModel
 
 import androidx.lifecycle.ViewModel
 import com.chobo.presentation.view.main.component.GoalReadingListOfBooksReadItemData
-import com.chobo.presentation.view.main.component.ReadingGoalGraphData
+import com.chobo.presentation.view.main.component.GoalReadingGraphData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class GoalReadingViewModel @Inject constructor() : ViewModel() {
     private val _goalBookRead = MutableStateFlow(0)
-    init {
-        _goalBookRead.value = 15
-    }
-    val goalBookRead: StateFlow<Int>
-        get() = _goalBookRead
+    val goalBookRead: StateFlow<Int> = _goalBookRead.asStateFlow()
 
     private val _goalReadingGraphDataList = MutableStateFlow(
         listOf(
@@ -28,35 +25,37 @@ class GoalReadingViewModel @Inject constructor() : ViewModel() {
             GoalReadingGraphData(1, 3, false, "오"),
         )
     )
-    val goalReadingGraphDataList: StateFlow<List<GoalReadingGraphData>> =
-        _goalReadingGraphDataList.asStateFlow()
+    val goalReadingGraphDataList: StateFlow<List<GoalReadingGraphData>> = _goalReadingGraphDataList.asStateFlow()
 
-    private val _goalReadingListOfBooksReadItemDataList = (
-            listOf(
-                GoalReadingListOfBooksReadItemData(
-                    12,
-                    12,
-                    "제목",
-                    "내용입니다 그렇ewjiofhowehfluieifnhcielhuferhwiegir습니다"
-                ),
-                GoalReadingListOfBooksReadItemData(
-                    12,
-                    12,
-                    "제목",
-                    "내용renwcilntinhvgdnghrerns입니다 그렇습니다"
-                ),
-                GoalReadingListOfBooksReadItemData(12, 12, "제목", "내용입니다 그렇습니다"),
-                GoalReadingListOfBooksReadItemData(12, 12, "제목", "내용입니다 그렇습니다"),
-                GoalReadingListOfBooksReadItemData(
-                    12,
-                    12,
-                    "제목",
-                    "내용fueiecgkenrgb hefvhjfrhfobhu a boifibdaf hfos hfoshfhdkldu PQWOPSJKFL;JASO UEFIPAJSOIFJ ;ASJF KDJA;FJP[iw jehfuahl;fj;lksjdpoaf jiojf'jwpeoirpanskj vjiocxjnv;jewlj fi0snlfj ajfnlk saklfnsa;lkj vpijcpxbf,wnlfndjakfpzujvo eaoji ofjeio fozisfoizs입니다 그렇습니다"
-                )
+    private val _goalReadingListOfBooksReadItemDataList = MutableStateFlow(
+        listOf(
+            GoalReadingListOfBooksReadItemData(
+                12,
+                12,
+                "제목",
+                "내용입니다 그렇ewjiofhowehfluieifnhcielhuferhwiegir습니다"
+            ),
+            GoalReadingListOfBooksReadItemData(
+                12,
+                12,
+                "제목",
+                "내용renwcilntinhvgdnghrerns입니다 그렇습니다"
+            ),
+            GoalReadingListOfBooksReadItemData(12, 12, "제목", "내용입니다 그렇습니다"),
+            GoalReadingListOfBooksReadItemData(12, 12, "제목", "내용입니다 그렇습니다"),
+            GoalReadingListOfBooksReadItemData(
+                12,
+                12,
+                "제목",
+                "내용fueiecgkenrgb hefvhjfrhfobhu a boifibdaf hfos hfoshfhdkldu PQWOPSJKFL;JASO UEFIPAJSOIFJ ;ASJF KDJA;FJP[iw jehfuahl;fj;lksjdpoaf jiojf'jwpeoirpanskj vjiocxjnv;jewlj fi0snlfj ajfnlk saklfnsa;lkj vpijcpxbf,wnlfndjakfpzujvo eaoji ofjeio fozisfoizs입니다 그렇습니다"
             )
-            )
-    val goalReadingListOfBooksReadItemDataList = _goalReadingListOfBooksReadItemDataList
+        )
+    )
+    val goalReadingListOfBooksReadItemDataList :StateFlow<List<GoalReadingListOfBooksReadItemData>> = _goalReadingListOfBooksReadItemDataList.asStateFlow()
 
+    init {
+        _goalBookRead.value = 15
+    }
 
     fun plusOnClick() {
 
