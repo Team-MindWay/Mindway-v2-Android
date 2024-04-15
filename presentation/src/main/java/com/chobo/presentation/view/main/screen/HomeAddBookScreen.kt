94bbb59d747a7454e5a92ac6ccb983847bb8be6b
명훈 <s23065@gsm.hs.kr>
2024-04-15 21:16:27 +0900

    :sparkles: :: 키보드가 올라왔을때 다른 화면을 누르면 키보드가 내려가게 하는 걸 구현했습니다!

--- presentation/src/main/java/com/chobo/presentation/view/main/screen/HomeAddBookScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/main/screen/HomeAddBookScreen.kt
@@ -1,6 +1,7 @@
 package com.chobo.presentation.view.main.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,10 +11,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,52 +37,59 @@ fun HomeAddBookScreen(
 ) {
     val titleTextState by homeAddBookViewModel.titleTextState.collectAsState()
     val contentTextState by homeAddBookViewModel.contentTextState.collectAsState()
+    val focusManager = LocalFocusManager.current
 
     MindWayAndroidTheme { colors, typography ->
-        Column(modifier = modifier.background(color = colors.WHITE)) {
-            Spacer(modifier = Modifier.height(20.dp))
-            AddBookTopAppBar(startIconOnClick = { navigateToBack() })
-            Column(
-                verticalArrangement = Arrangement.SpaceBetween,
-                horizontalAlignment = Alignment.Start,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .imePadding()
+        CompositionLocalProvider(LocalFocusManager provides focusManager) {
+            Column(modifier = modifier
+                .background(color = colors.WHITE)
+                .pointerInput(Unit) {
+                    detectTapGestures {
+                        focusManager.clearFocus()
+                    }
+                }
             ) {
+                Spacer(modifier = Modifier.height(20.dp))
+                AddBookTopAppBar(startIconOnClick = { navigateToBack() })
                 Column(
-                    verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
+                    verticalArrangement = Arrangement.SpaceBetween,
                     horizontalAlignment = Alignment.Start,
-                    modifier = Modifier
-                        .padding(
-                            horizontal = 24.dp,
-                            vertical = 28.dp
-                        )
-                        .fillMaxWidth()
-                ) {
-                    MindWayTextField(
-                        title = stringResource(R.string.title),
-                        textState = titleTextState,
-                        placeholder = stringResource(R.string.please_enter_the_book_title),
-                        errorMessage = stringResource(R.string.error_title),
-                        lengthLimit = homeAddBookViewModel.titleTextMaxLength,
-                        updateTextValue = homeAddBookViewModel::updateTitleTextState
-                    )
-                    MindWayTextField(
-                        title = stringResource(R.string.content),
-                        textState = contentTextState,
-                        placeholder = stringResource(R.string.please_enter_the_book_content),
-                        errorMessage = stringResource(R.string.error_content),
-                        lengthLimit = homeAddBookViewModel.contentTextMaxLength,
-                        updateTextValue = homeAddBookViewModel::updateContentTextState
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                    MindWayButton(
-                        text = stringResource(R.string.check),
+                    modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
+                        horizontalAlignment = Alignment.Start,
                         modifier = Modifier
+                            .padding(
+                                horizontal = 24.dp,
+                                vertical = 28.dp
+                            )
                             .fillMaxWidth()
-                            .height(60.dp),
-                        onClick = { homeAddBookViewModel.checkButton() }
-                    )
+                    ) {
+                        MindWayTextField(
+                            title = stringResource(R.string.title),
+                            textState = titleTextState,
+                            placeholder = stringResource(R.string.please_enter_the_book_title),
+                            errorMessage = stringResource(R.string.error_title),
+                            lengthLimit = homeAddBookViewModel.titleTextMaxLength,
+                            updateTextValue = homeAddBookViewModel::updateTitleTextState
+                        )
+                        MindWayTextField(
+                            title = stringResource(R.string.content),
+                            textState = contentTextState,
+                            placeholder = stringResource(R.string.please_enter_the_book_content),
+                            errorMessage = stringResource(R.string.error_content),
+                            lengthLimit = homeAddBookViewModel.contentTextMaxLength,
+                            updateTextValue = homeAddBookViewModel::updateContentTextState
+                        )
+                        Spacer(modifier = Modifier.weight(1f))
+                        MindWayButton(
+                            text = stringResource(R.string.check),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(60.dp),
+                            onClick = { homeAddBookViewModel.checkButton() }
+                        )
+                    }
                 }
             }
         }
