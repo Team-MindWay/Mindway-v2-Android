7748be583b26e9d396d1f17ef8c651f12c0115a9
명훈 <s23065@gsm.hs.kr>
2024-04-15 15:16:44 +0900

    :fire: :: Fire unused import

--- presentation/src/main/java/com/chobo/presentation/view/book/screen/BookScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/book/screen/BookScreen.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
