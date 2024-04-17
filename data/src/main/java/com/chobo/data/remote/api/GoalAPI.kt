8fc5d40d04cef9876d8b7e0724aecfa59812d320
혜성 <118062596+answad@users.noreply.github.com>
2024-04-17 09:55:52 +0900

    :art: :: 파일구조 변경

--- data/src/main/java/com/chobo/data/remote/api/GoalAPI.kt
+++ data/src/main/java/com/chobo/data/remote/api/GoalAPI.kt
@@ -1,8 +1,8 @@
 package com.chobo.data.remote.api
 
 import com.chobo.data.remote.dto.order_request.OrderRequestBookType
-import com.chobo.data.remote.dto.request.goal.GoalGetRequestBodyGet
-import com.chobo.data.remote.dto.response.goal.GoalResponse
+import com.chobo.data.remote.dto.goal.request.GoalGetRequestBodyGet
+import com.chobo.data.remote.dto.goal.response.GoalResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -11,7 +11,6 @@ import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
-import java.lang.reflect.Parameter
 
 interface GoalAPI {
     @POST("/api/v2/goal")
