47dc1cf0fa4e80c23af8483565091acbc0a833e9
혜성 <118062596+answad@users.noreply.github.com>
2024-04-17 19:55:20 +0900

    :sparkles: :: GoalAPI 잘못 작성한 부분 삭제, GoalAPI 구현

--- data/src/main/java/com/chobo/data/remote/api/GoalAPI.kt
+++ data/src/main/java/com/chobo/data/remote/api/GoalAPI.kt
@@ -1,35 +1,17 @@
 package com.chobo.data.remote.api
 
-import com.chobo.data.remote.enumtype.OrderRequestBookType
-import com.chobo.data.remote.dto.goal.request.GoalGetRequestBodyGet
+import com.chobo.data.remote.dto.goal.request.GoalRequestBodyPost
 import com.chobo.data.remote.dto.goal.response.GoalResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface GoalAPI {
-    @POST("/api/v2/goal")
-    fun goalPost()
-
-    @PATCH("/api/v2/goal/{order_id}")
-    fun goalPatch(
-        @Body body: GoalGetRequestBodyGet,
-        @Path("order_id") orderId: String,
-        @Query("type") type: OrderRequestBookType
+    @POST("/api/v2/order")
+    suspend fun orderPost(
+        @Body body: GoalRequestBodyPost
     )
 
-    @GET("/api/v2/goal/{order_id}")
-    fun goalDelete(
-        @Path("order_id") orderId: String
-    )
-
-    @GET("/api/v2/goal")
-    suspend fun goalGet(
-        @Header("RefreshToken") refreshToken: String,
-        @Body body: GoalGetRequestBodyGet,
-    ): GoalResponse
+    @GET("/api/v2/order")
+    suspend fun orderGet(): GoalResponse
 }
