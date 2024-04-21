596ca3918a1e315c43c7b621b6905e856461e057
명훈 <s23065@gsm.hs.kr>
2024-04-22 08:05:22 +0900

    :sparkles: :: Setting RemoteDataSourceModule, RepositoryModule

---
+++ app/src/main/java/com/chobo/mindway_v2_android/Module/RemoteDataSourceModule.kt
@@ -0,0 +1,11 @@
+package com.chobo.mindway_v2_android.Module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+annotation class RemoteDataSourceModule {
+
+}
