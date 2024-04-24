def460c3b3ac4bd45ef02e2331e64aae3dcfd6d0
혜성 <118062596+answad@users.noreply.github.com>
2024-04-24 20:27:25 +0900

    :sparkles: :: HomeScreen 에 clickableSingle 적용

--- presentation/src/main/java/com/chobo/presentation/view/component/button/MindWayButton.kt
+++ presentation/src/main/java/com/chobo/presentation/view/component/button/MindWayButton.kt
@@ -1,8 +1,6 @@
 package com.chobo.presentation.view.component.button
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,6 +13,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chobo.presentation.view.component.multipleEventsCutterManager.clickableSingle
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
 import com.chobo.presentation.view.theme.color.MindWayColor
 
@@ -26,13 +25,7 @@ fun MindWayButton(
     isClickable: Boolean = true,
     onClick: () -> Unit,
 ) {
-    val clickableModifier =
-        if (isClickable) modifier.clickable(
-            interactionSource = MutableInteractionSource(),
-            indication = null
-        ) { onClick() }
-
-        else modifier
+    val clickableModifier = if (isClickable) modifier.clickableSingle{ onClick() } else modifier
 
     MindWayAndroidTheme { colors, typography ->
         Row(
