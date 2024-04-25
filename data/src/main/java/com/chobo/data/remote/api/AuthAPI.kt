94840e1337e86db1c164811386186aa46907c665
명훈 <s23065@gsm.hs.kr>
2024-04-26 08:47:44 +0900

    ♻️ :: ModifyAuthAPI(EndPoint)

--- data/src/main/java/com/chobo/data/remote/api/AuthAPI.kt
+++ data/src/main/java/com/chobo/data/remote/api/AuthAPI.kt
@@ -8,7 +8,7 @@ import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthAPI {
-    @POST("/api/v2/auth/signin")
+    @POST("/api/v2/auth")
     suspend fun gAuthLogin(
         @Body body: GAuthLoginRequestBody
     ): GAuthLoginResponse
