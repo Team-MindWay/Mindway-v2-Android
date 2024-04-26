ee16b1a12157884f4ee818a8100ddf3e1fee1fc4
혜성 <118062596+answad@users.noreply.github.com>
2024-04-26 14:59:49 +0900

    :lipstick: :: toast 적용

--- presentation/src/main/java/com/chobo/presentation/view/my/screen/MyScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/my/screen/MyScreen.kt
@@ -1,18 +1,25 @@
 package com.chobo.presentation.view.my.screen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -28,12 +35,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.R
+import com.chobo.presentation.view.component.customToast.MindWayToast
 import com.chobo.presentation.view.my.component.MyBookDeletePopUp
 import com.chobo.presentation.view.my.component.MyBookListItem
 import com.chobo.presentation.view.my.component.MyNameCard
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
 import com.chobo.presentation.viewModel.MyBookEditViewModel
 import com.chobo.presentation.viewModel.MyViewModel
+import kotlinx.coroutines.delay
 
 @Composable
 fun MyScreen(
@@ -45,77 +54,105 @@ fun MyScreen(
 ) {
     val myName by myViewModel.myName.collectAsState()
     val myBookListItemDataList by myViewModel.myBookListItemDataList.collectAsState()
+    val isToastVisible by myViewModel.isToastVisible.collectAsState()
     var bookDeleteDialog by remember { mutableStateOf(false) }
     var selectedIndex by remember { mutableIntStateOf(-1) }
     var selectedBookTitle by remember { mutableStateOf("") }
 
     MindWayAndroidTheme { colors, typography ->
-        Column(modifier = modifier.background(color = colors.WHITE)) {
-            if (bookDeleteDialog) {
-                Dialog(onDismissRequest = { bookDeleteDialog = false }) {
-                    MyBookDeletePopUp(
-                        title = selectedBookTitle,
-                        cancelOnclick = {
-                            bookDeleteDialog = false
-                            selectedBookTitle = ""
-                        },
-                        checkOnclick = {
-                            if (selectedIndex != -1) {
-                                myViewModel.removeBookItem(selectedIndex)
-                                selectedIndex = -1
+        Box(modifier = modifier.background(color = colors.WHITE)) {
+            Column {
+                if (bookDeleteDialog) {
+                    Dialog(onDismissRequest = { bookDeleteDialog = false }) {
+                        MyBookDeletePopUp(
+                            title = selectedBookTitle,
+                            cancelOnclick = {
+                                bookDeleteDialog = false
+                                selectedBookTitle = ""
+                            },
+                            checkOnclick = {
+                                if (selectedIndex != -1) {
+                                    myViewModel.removeBookItem(selectedIndex)
+                                    selectedIndex = -1
+                                }
+                                bookDeleteDialog = false
+                                selectedBookTitle = ""
                             }
-                            bookDeleteDialog = false
-                            selectedBookTitle = ""
-                        }
+                        )
+                    }
+                }
+                Spacer(modifier = Modifier.height(43.dp))
+                MyNameCard(
+                    name = myName,
+                    onClick = onClick,
+                )
+                Row(
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = stringResource(R.string.book_request_list),
+                        style = typography.labelLarge,
+                        fontWeight = FontWeight.Normal,
+                        color = colors.GRAY400,
                     )
                 }
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(
+                            horizontal = 24.dp,
+                            vertical = 16.dp
+                        )
+                ) {
+                    itemsIndexed(myBookListItemDataList) { index, item ->
+                        MyBookListItem(
+                            title = item.title,
+                            writer = item.writer,
+                            editOnclick = {
+                                myBookEditViewModel.initValue(index = index)
+                                navigateToMyBookEdit()
+                            },
+                            trashCanOnclick = {
+                                item.trashCanOnclick
+                                bookDeleteDialog = true
+                                selectedIndex = index
+                                selectedBookTitle = item.title
+                            }
+                        )
+                    }
+                }
             }
-            Spacer(modifier = Modifier.height(43.dp))
-            MyNameCard(
-                name = myName,
-                onClick = onClick,
-            )
-            Row(
-                horizontalArrangement = Arrangement.Start,
-                verticalAlignment = Alignment.CenterVertically,
+            AnimatedVisibility(
+                visible = isToastVisible,
                 modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .offset(y = (-50).dp)
+                    .padding(horizontal = 24.dp),
+                enter = slideInVertically(
+                    initialOffsetY = { it + 110 },
+                    animationSpec = tween(durationMillis = 500)
+                ),
+                exit = slideOutVertically(
+                    targetOffsetY = { it + 110 },
+                    animationSpec = tween(durationMillis = 500)
+                )
             ) {
-                Text(
-                    text = stringResource(R.string.book_request_list),
-                    style = typography.labelLarge,
-                    fontWeight = FontWeight.Normal,
-                    color = colors.GRAY400,
+                MindWayToast(
+                    isSuccess = true,
+                    text = stringResource(R.string.book_request_succes_toast),
+                    modifier = Modifier.fillMaxWidth()
                 )
             }
-            LazyColumn(
-                verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(
-                        horizontal = 24.dp,
-                        vertical = 16.dp
-                    )
-            ) {
-                itemsIndexed(myBookListItemDataList) { index, item ->
-                    MyBookListItem(
-                        title = item.title,
-                        writer = item.writer,
-                        editOnclick = {
-                            myBookEditViewModel.initValue(index = index)
-                            navigateToMyBookEdit()
-                        },
-                        trashCanOnclick = {
-                            item.trashCanOnclick
-                            bookDeleteDialog = true
-                            selectedIndex = index
-                            selectedBookTitle = item.title
-                        }
-                    )
-                }
-            }
         }
+    }
+    LaunchedEffect(isToastVisible) {
+        delay(2000)
+        myViewModel.toggleIsToastVisible()
     }
 }
 
