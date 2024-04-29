8c2a87b492500946406b9da6e77bce75389ef79d
명훈 <s23065@gsm.hs.kr>
2024-04-29 15:40:22 +0900

    :fire: :: Fire Unused Import

--- presentation/src/main/java/com/chobo/presentation/view/login/screen/LoginScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/login/screen/LoginScreen.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
