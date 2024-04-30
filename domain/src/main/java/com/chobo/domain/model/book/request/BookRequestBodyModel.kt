d8d38f43905e3a50257cb63e9a2bc54a0a2811c2
혜성 <118062596+answad@users.noreply.github.com>
2024-04-30 16:11:00 +0900

    :sparkles: :: domain에 model들 추가

---
+++ domain/src/main/java/com/chobo/domain/model/book/request/BookRequestBodyModel.kt
@@ -0,0 +1,6 @@
+package com.chobo.domain.model.book.request
+
+data class BookRequestBodyModel(
+    val title: String,
+    val plot: String
+)
---
+++ domain/src/main/java/com/chobo/domain/model/book/response/BookListResponseModel.kt
@@ -0,0 +1,7 @@
+package com.chobo.domain.model.book.response
+
+data class BookListResponseModel(
+    val title: String,
+    val plot: String,
+    val created_at: String
+)
