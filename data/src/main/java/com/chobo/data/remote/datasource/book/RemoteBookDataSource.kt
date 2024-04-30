6ab9fe9c9ebd2a2554182f7db2bc214b7a6a9e51
혜성 <118062596+answad@users.noreply.github.com>
2024-04-30 16:10:30 +0900

    :sparkles: :: API 구현 RemoteBookDataSource

---
+++ data/src/main/java/com/chobo/data/remote/datasource/book/RemoteBookDataSource.kt
@@ -0,0 +1,18 @@
+package com.chobo.data.remote.datasource.book
+
+import com.chobo.data.remote.dto.book.request.BookRequestBody
+import com.chobo.data.remote.dto.book.response.BookListResponse
+import kotlinx.coroutines.flow.Flow
+
+interface RemoteBookDataSource {
+    suspend fun bookPost(body: BookRequestBody): Flow<Unit>
+
+    suspend fun bookGet(): Flow<List<BookListResponse>>
+
+    suspend fun bookPatch(
+        body: BookRequestBody,
+        bookId: Long
+    ): Flow<Unit>
+
+    suspend fun bookDelete(bookId: Long): Flow<Unit>
+}
---
+++ data/src/main/java/com/chobo/data/remote/datasource/book/RemoteBookDataSourceImpl.kt
@@ -0,0 +1,50 @@
+package com.chobo.data.remote.datasource.book
+
+import com.chobo.data.remote.api.BookAPI
+import com.chobo.data.remote.dto.book.request.BookRequestBody
+import com.chobo.data.remote.dto.book.response.BookListResponse
+import com.chobo.data.util.MindWayAPIHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class RemoteBookDataSourceImpl @Inject constructor(
+    private val bookAPI: BookAPI
+) : RemoteBookDataSource {
+    override suspend fun bookPost(body: BookRequestBody): Flow<Unit> = flow {
+        emit(
+            MindWayAPIHandler<Unit>()
+                .httpRequest { bookAPI.bookPost(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun bookGet(): Flow<List<BookListResponse>> = flow {
+        emit(
+            MindWayAPIHandler<List<BookListResponse>>()
+                .httpRequest { bookAPI.bookGet() }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun bookPatch(
+        body: BookRequestBody,
+        bookId: Long
+    ): Flow<Unit> = flow {
+        emit(
+            MindWayAPIHandler<Unit>()
+                .httpRequest { bookAPI.bookPatch(body = body, bookId = bookId) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun bookDelete(bookId: Long): Flow<Unit> = flow {
+        emit(
+            MindWayAPIHandler<Unit>()
+                .httpRequest { bookAPI.bookDelete(bookId = bookId) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+}
