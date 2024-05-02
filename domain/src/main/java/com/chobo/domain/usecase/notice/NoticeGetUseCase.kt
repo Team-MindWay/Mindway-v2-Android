719b68bd471dad2903d6faca2db7effdbf11962a
혜성 <118062596+answad@users.noreply.github.com>
2024-05-02 10:31:45 +0900

    :truck: :: GetNoticeUseCase.kt -> NoticeGetUseCase 이름을 사용하기 쉽도록 변경

--- domain/src/main/java/com/chobo/domain/usecase/notice/NoticeGetUseCase.kt
+++ domain/src/main/java/com/chobo/domain/usecase/notice/NoticeGetUseCase.kt
@@ -3,7 +3,7 @@ package com.chobo.domain.usecase.notice
 import com.chobo.domain.repository.NoticeRepository
 import javax.inject.Inject
 
-class GetNoticeUseCase @Inject constructor(
+class NoticeGetUseCase @Inject constructor(
     private val noticeRepository: NoticeRepository
 ) {
     suspend operator fun invoke() = runCatching {
