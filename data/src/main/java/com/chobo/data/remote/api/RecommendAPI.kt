9e07497997f5b717295289763d86d95c3cdfb6ba
명훈 <s23065@gsm.hs.kr>
2024-05-02 10:11:20 +0900

    ::memo: :: Add @Query

--- data/src/main/java/com/chobo/data/remote/api/RecommendAPI.kt
+++ data/src/main/java/com/chobo/data/remote/api/RecommendAPI.kt
@@ -1,26 +1,32 @@
 package com.chobo.data.remote.api
 
-import com.chobo.data.remote.dto.recommend.RecommendAll
+import com.chobo.data.remote.dto.recommend.request.RecommendAllRequest
+import com.chobo.data.remote.dto.recommend.response.RecommendAllResponse
+import com.chobo.data.remote.enumtype.OrderRequestBookType
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RecommendAPI {
 
     @POST("/api/v2/recommend")
     suspend fun postRecommendBook(
-        @Body body: RecommendAll
+        @Body body: RecommendAllRequest,
+        @Query("type") type: OrderRequestBookType
     )
 
     @GET("/api/v2/recommend")
-    suspend fun getRecommendBook(): RecommendAll
+    suspend fun getRecommendBook(
+        @Query("type") type: OrderRequestBookType
+    ): RecommendAllResponse
 
     @PATCH("/api/v2/recommend/{rec_id}")
     suspend fun patchRecommendBook(
-        @Body body: RecommendAll,
+        @Body body: RecommendAllRequest,
         @Path("rec_id") id: Long
     )
 
