e626a15ee17df5b1ddb3a4c6b523a6399ea78eab
명훈 <s23065@gsm.hs.kr>
2024-05-02 11:39:50 +0900

    :sparkles: :: RecommendUseCae

---
+++ domain/src/main/java/com/chobo/domain/usecase/recommend/GetRecommendBookUseCase.kt
@@ -0,0 +1,14 @@
+package com.chobo.domain.usecase.recommend
+
+import com.chobo.domain.repository.RecommendRepository
+import javax.inject.Inject
+
+class GetRecommendBookUseCase @Inject constructor(
+    private val recommendRepository: RecommendRepository
+) {
+    suspend operator fun invoke(type: String) = runCatching {
+        recommendRepository.getRecommendBook(
+            type = type
+        )
+    }
+}
