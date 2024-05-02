fdbe33e28ec0d87192a99284dac2e34b165c4342
명훈 <s23065@gsm.hs.kr>
2024-05-02 10:14:09 +0900

    :sparkles: :: Add RemoteRecommendDataSource to RemoteDataSourceModule

--- app/src/main/java/com/chobo/mindway_v2_android/Module/RemoteDataSourceModule.kt
+++ app/src/main/java/com/chobo/mindway_v2_android/Module/RemoteDataSourceModule.kt
@@ -4,6 +4,8 @@ import com.chobo.data.remote.datasource.auth.RemoteAuthDataSource
 import com.chobo.data.remote.datasource.auth.RemoteAuthDataSourceImpl
 import com.chobo.data.remote.datasource.book.RemoteBookDataSource
 import com.chobo.data.remote.datasource.book.RemoteBookDataSourceImpl
+import com.chobo.data.remote.datasource.recommend.RemoteRecommendDataSource
+import com.chobo.data.remote.datasource.recommend.RemoteRecommendDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,4 +23,9 @@ abstract class RemoteDataSourceModule {
     abstract fun provideRemoteBookDataSource(
         remoteBookDataSourceImpl: RemoteBookDataSourceImpl
     ): RemoteBookDataSource
+
+    @Binds
+    abstract fun provideRemoteRecommendDataSource(
+        remoteRecommendDataSourceImpl: RemoteRecommendDataSourceImpl
+    ): RemoteRecommendDataSource
 }
