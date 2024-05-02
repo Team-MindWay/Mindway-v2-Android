a59873505ccb2214c239092ddcbaa230469e4077
혜성 <118062596+answad@users.noreply.github.com>
2024-05-02 10:03:31 +0900

    :sparkles: ::  NoticeAll에 toModel추가

--- data/src/main/java/com/chobo/data/remote/dto/notice/NoticeAll.kt
+++ data/src/main/java/com/chobo/data/remote/dto/notice/NoticeAll.kt
@@ -1,5 +1,6 @@
 package com.chobo.data.remote.dto.notice
 
+import com.chobo.domain.model.notice.NoticeAllModel
 import com.google.gson.annotations.SerializedName
 
 data class NoticeAll(
@@ -7,4 +8,9 @@ data class NoticeAll(
     val title: String,
     @SerializedName("content")
     val content: String
+)
+
+fun NoticeAll.toModel(): NoticeAllModel = NoticeAllModel(
+    title = title,
+    content = content,
 )
