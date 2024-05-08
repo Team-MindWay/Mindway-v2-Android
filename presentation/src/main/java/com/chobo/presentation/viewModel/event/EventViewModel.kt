d58e4f782616d41d6f19c32e6a3246925b01b48a
명훈 <s23065@gsm.hs.kr>
2024-05-09 08:46:56 +0900

    :sparkles: :: Create EventViewModel

--- presentation/src/main/java/com/chobo/presentation/viewModel/event/EventViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/event/EventViewModel.kt
@@ -2,11 +2,15 @@ package com.chobo.presentation.viewModel.event
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.chobo.presentation.viewModel.util.result.Result
-import com.chobo.presentation.viewModel.util.result.asResult
+import com.chobo.domain.usecase.event.GetDetailEventUseCase
+import com.chobo.domain.usecase.event.GetEventDateListUseCase
 import com.chobo.domain.usecase.event.GetEventListUseCase
 import com.chobo.presentation.view.event.component.EventsData
+import com.chobo.presentation.viewModel.event.uistate.GetDetailEventUiState
+import com.chobo.presentation.viewModel.event.uistate.GetEventDateListUiState
 import com.chobo.presentation.viewModel.event.uistate.GetEventListUiState
+import com.chobo.presentation.viewModel.util.result.Result
+import com.chobo.presentation.viewModel.util.result.asResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,16 +21,24 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EventViewModel @Inject constructor(
-    private val getEventListUseCase: GetEventListUseCase
+    private val getEventListUseCase: GetEventListUseCase,
+    private val getEventDetailUseCase: GetDetailEventUseCase,
+    private val getEventDateListUseCase: GetEventDateListUseCase,
 ) : ViewModel() {
-    private val _getEventListUiState = MutableStateFlow<GetEventListUiState>(GetEventListUiState.Loading)
-    val getEventListUiState = _getEventListUiState.asStateFlow()
-
     private val _currentEventsDataList = MutableStateFlow<List<EventsData>>(listOf())
     val currentEventsDataList: StateFlow<List<EventsData>> = _currentEventsDataList.asStateFlow()
 
     private val _pastEventsDataList = MutableStateFlow<List<EventsData>>(listOf())
     val pastEventsDataList: StateFlow<List<EventsData>> = _pastEventsDataList.asStateFlow()
+
+    private val _getEventListUiState = MutableStateFlow<GetEventListUiState>(GetEventListUiState.Loading)
+    val getEventListUiState = _getEventListUiState.asStateFlow()
+
+    private val _getDetailEventUiState = MutableStateFlow<GetDetailEventUiState>(GetDetailEventUiState.Loading)
+    val getDetailEventUiState = _getDetailEventUiState.asStateFlow()
+
+    private val _getEventDateListUiState = MutableStateFlow<GetEventDateListUiState>(GetEventDateListUiState.Loading)
+    val getEventDateListUiState = _getEventDateListUiState.asStateFlow()
 
     fun getEventList(status: String) = viewModelScope.launch {
         getEventListUseCase(status = status)
@@ -40,6 +52,37 @@ class EventViewModel @Inject constructor(
                         _getEventListUiState.value = GetEventListUiState.Success(result.data)
                     }
                     is Result.Fail -> _getEventListUiState.value = GetEventListUiState.Fail(result.exception)
+                }
+            }
+    }
+
+    fun getDetailEvent(eventId: Long) = viewModelScope.launch {
+        getEventDetailUseCase(eventId = eventId)
+            .asResult()
+            .collectLatest { result ->
+                when(result) {
+                    is Result.Loading -> _getDetailEventUiState.value = GetDetailEventUiState.Loading
+                    is Result.Success -> _getDetailEventUiState.value = GetDetailEventUiState.Success(result.data)
+                    is Result.Fail -> _getDetailEventUiState.value = GetDetailEventUiState.Fail(result.exception)
+                }
+            }
+    }
+
+    fun getEventDataList(date: String) = viewModelScope.launch {
+        getEventDateListUseCase(date = date)
+            .asResult()
+            .collectLatest { result ->
+                when(result) {
+                    is Result.Loading -> _getEventDateListUiState.value = GetEventDateListUiState.Loading
+                    is Result.Success -> {
+                        if (result.data.isEmpty()) {
+                            _getEventDateListUiState.value = GetEventDateListUiState.Empty
+                        } else {
+                            _getEventDateListUiState.value =
+                                GetEventDateListUiState.Success(result.data)
+                        }
+                    }
+                    is Result.Fail -> _getEventDateListUiState.value = GetEventDateListUiState.Fail(result.exception)
                 }
             }
     }
