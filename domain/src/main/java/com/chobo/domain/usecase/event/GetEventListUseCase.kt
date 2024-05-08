39d8c94507e8615ddde777542c1729ea78f00d18
명훈 <s23065@gsm.hs.kr>
2024-05-08 11:29:15 +0900

    :memo: :: Resolve Recevier type mismatch(runCatching -> Flow)

--- domain/src/main/java/com/chobo/domain/usecase/event/GetEventListUseCase.kt
+++ domain/src/main/java/com/chobo/domain/usecase/event/GetEventListUseCase.kt
@@ -1,12 +1,13 @@
 package com.chobo.domain.usecase.event
 
+import com.chobo.domain.model.event.response.GetEventListResponseModel
 import com.chobo.domain.repository.EventRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetEventListUseCase @Inject constructor(
     private val eventRepository: EventRepository
 ) {
-    suspend operator fun invoke(status: String) = runCatching {
+    suspend operator fun invoke(status: String): Flow<List<GetEventListResponseModel>> =
         eventRepository.getEventList(status = status)
-    }
 }
