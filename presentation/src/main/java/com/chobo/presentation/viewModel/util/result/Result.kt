1a85128e55a224f75e224efce372a9fe693d1b27
명훈 <s23065@gsm.hs.kr>
2024-05-08 10:40:29 +0900

    :sparkles: :: Create Result(viewmodel - util)

---
+++ presentation/src/main/java/com/chobo/presentation/viewModel/util/result/Result.kt
@@ -0,0 +1,16 @@
+package com.chobo.presentation.viewModel.util.result
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+
+sealed interface Result<out T> {
+    data class Success<T>(val data: T): Result<T>
+    data class Fail(val exception: Throwable): Result<Nothing>
+    object Loading: Result<Nothing>
+}
+
+fun <T> Flow<T>.asResult(): Flow<Result<T>> = map<T, Result<T>> { Result.Success(it) }
+    .onStart { emit(Result.Loading) }
+    .catch { emit(Result.Fail(it)) }
