97d198ed1aedf7a247a6268f324deba0fc92301d
명훈 <s23065@gsm.hs.kr>
2024-05-09 08:35:05 +0900

    :memo: :: Changed UseCase Receiver Type

--- domain/src/main/java/com/chobo/domain/usecase/event/GetEventDateListUseCase.kt
+++ domain/src/main/java/com/chobo/domain/usecase/event/GetEventDateListUseCase.kt
@@ -1,12 +1,13 @@
 package com.chobo.domain.usecase.event
 
+import com.chobo.domain.model.event.response.GetEventDateListResponseModel
 import com.chobo.domain.repository.EventRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetEventDateListUseCase @Inject constructor(
     private val eventRepository: EventRepository
 ) {
-    suspend operator fun invoke(date: String) = runCatching {
+    suspend operator fun invoke(date: String): Flow<List<GetEventDateListResponseModel>> =
         eventRepository.getEventDateList(date = date)
-    }
 }
