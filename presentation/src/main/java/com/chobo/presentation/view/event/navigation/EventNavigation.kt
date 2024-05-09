614a9b00a4076b9e9f56b1c468604e317f899e3d
혜성 <118062596+answad@users.noreply.github.com>
2024-05-09 14:55:49 +0900

    :fire: :: navigationToEvent삭제

--- presentation/src/main/java/com/chobo/presentation/view/event/navigation/EventNavigation.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/navigation/EventNavigation.kt
@@ -3,18 +3,9 @@ package com.chobo.presentation.view.event.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.chobo.presentation.view.event.screen.DetailEventScreen
-import com.chobo.presentation.view.event.screen.EventScreen
-import com.chobo.presentation.viewModel.event.uistate.GetDetailEventUiState
-import com.chobo.presentation.viewModel.event.uistate.GetEventDateListUiState
-import com.chobo.presentation.viewModel.event.uistate.GetEventListUiState
+import com.chobo.presentation.view.event.screen.DetailEventRoute
 
-const val eventRoute = "event_route"
 const val detailEventRoute = "detail_event_route"
-
-fun NavController.navigationToEvent() {
-    this.navigate(eventRoute)
-}
 
 fun NavController.navigationToDetailEvent() {
     this.navigate(detailEventRoute)
