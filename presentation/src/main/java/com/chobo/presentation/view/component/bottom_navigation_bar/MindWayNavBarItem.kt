d172141fbf6ba3b9e218d3060f1556fef8cf5110
혜성 <118062596+answad@users.noreply.github.com>
2024-05-09 23:28:00 +0900

    :technologist: :: 반복코드 삭제

--- presentation/src/main/java/com/chobo/presentation/view/component/bottom_navigation_bar/MindWayNavBarItem.kt
+++ presentation/src/main/java/com/chobo/presentation/view/component/bottom_navigation_bar/MindWayNavBarItem.kt
@@ -14,10 +14,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.chobo.presentation.R
-import com.chobo.presentation.view.component.icon.BookIcon
-import com.chobo.presentation.view.component.icon.HeartIcon
+import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.BOOKS
+import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.EVENT
+import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.HOME
+import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.MY
 import com.chobo.presentation.view.component.icon.HomeIcon
-import com.chobo.presentation.view.component.icon.ProfileIcon
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
 
 @Composable
@@ -27,74 +28,24 @@ fun MindWayNavBarItem(
     isSelected: Boolean
 ) {
     MindWayAndroidTheme { colors, typography ->
-        when (type) {
-            MindWayNavBarItemType.HOME -> {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    HomeIcon(modifier = modifier, isSelected = isSelected)
-                    Spacer(modifier = modifier.height(4.dp))
-                    Text(
-                        text = stringResource(id = R.string.home),
-                        style = typography.labelLarge,
-                        fontWeight = FontWeight.Medium,
-                        fontSize = 11.sp,
-                        color = if (isSelected) colors.Black else colors.GRAY400
-                    )
-                }
-            }
-
-            MindWayNavBarItemType.EVENT -> {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    HeartIcon(modifier = modifier, isSelected = isSelected)
-                    Spacer(modifier = modifier.height(4.dp))
-                    Text(
-                        text = stringResource(id = R.string.event),
-                        style = typography.labelLarge,
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 14.sp,
-                        color = if (isSelected) colors.Black else colors.GRAY400
-                    )
-                }
-            }
-
-            MindWayNavBarItemType.BOOKS -> {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    BookIcon(modifier = modifier, isSelected = isSelected)
-                    Spacer(modifier = modifier.height(4.dp))
-                    Text(
-                        text = stringResource(id = R.string.books),
-                        style = typography.labelLarge,
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 14.sp,
-                        color = if (isSelected) colors.Black else colors.GRAY400
-                    )
-                }
-            }
-
-            MindWayNavBarItemType.MY -> {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    ProfileIcon(modifier = modifier, isSelected = isSelected)
-                    Spacer(modifier = modifier.height(4.dp))
-                    Text(
-                        text = stringResource(id = R.string.my),
-                        style = typography.labelLarge,
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 14.sp,
-                        color = if (isSelected) colors.Black else colors.GRAY400
-                    )
-                }
-            }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            HomeIcon(modifier = modifier, isSelected = isSelected)
+            Spacer(modifier = modifier.height(4.dp))
+            Text(
+                text = when (type) {
+                    HOME -> stringResource(id = R.string.home)
+                    EVENT -> stringResource(id = R.string.event)
+                    BOOKS -> stringResource(id = R.string.books)
+                    MY -> stringResource(id = R.string.my)
+                },
+                style = typography.labelLarge,
+                fontWeight = FontWeight.Medium,
+                fontSize = 11.sp,
+                color = if (isSelected) colors.Black else colors.GRAY400
+            )
         }
     }
 }
@@ -103,7 +54,7 @@ fun MindWayNavBarItem(
 @Composable
 fun MindWayNavBarItemPreview() {
     MindWayNavBarItem(
-        type = MindWayNavBarItemType.HOME,
+        type = HOME,
         isSelected = true
     )
 }
