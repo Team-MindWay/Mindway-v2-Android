97a4b4da6a912755ea48d70466f677ac8ad91179
혜성 <118062596+answad@users.noreply.github.com>
2024-05-09 15:00:04 +0900

    :sparkles: :: NavGraphBuilder.detailEventScreen에  Route적용

--- presentation/src/main/java/com/chobo/presentation/view/event/navigation/EventNavigation.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/navigation/EventNavigation.kt
@@ -11,23 +11,10 @@ fun NavController.navigationToDetailEvent() {
     this.navigate(detailEventRoute)
 }
 
-fun NavGraphBuilder.eventScreen(
-    navigateToDetailEvent: () -> Unit
-) {
-    composable(eventRoute) {
-        EventScreen(
-            navigateToDetailEvent = navigateToDetailEvent,
-            getEventListUiState = GetEventListUiState.Loading,
-            getDetailEventUiState = GetDetailEventUiState.Loading,
-            getEventDateListUiState = GetEventDateListUiState.Loading,
-            mainCallBack = {  })
-    }
-}
-
 fun NavGraphBuilder.detailEventScreen(
     navigateToBack: () -> Unit
 ) {
     composable(detailEventRoute) {
-        DetailEventScreen(navigateToBack = navigateToBack)
+        DetailEventRoute(navigateToBack = navigateToBack)
     }
 }
