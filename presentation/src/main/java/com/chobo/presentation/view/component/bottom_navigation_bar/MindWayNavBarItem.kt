dcdd8040857998b6ac6c77f248d07a13a60aefbf
혜성 <118062596+answad@users.noreply.github.com>
2024-05-10 10:17:32 +0900

    :zap: :: colors, text, icon을 주입받도록 변경

--- presentation/src/main/java/com/chobo/presentation/view/component/bottom_navigation_bar/MindWayNavBarItem.kt
+++ presentation/src/main/java/com/chobo/presentation/view/component/bottom_navigation_bar/MindWayNavBarItem.kt
@@ -8,43 +8,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.chobo.presentation.R
-import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.BOOKS
-import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.EVENT
-import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.HOME
-import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.MY
 import com.chobo.presentation.view.component.icon.HomeIcon
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
 
 @Composable
 fun MindWayNavBarItem(
     modifier: Modifier = Modifier,
-    type: MindWayNavBarItemType,
-    isSelected: Boolean
+    colors: Color,
+    text: String,
+    icon: @Composable () -> Unit
 ) {
-    MindWayAndroidTheme { colors, typography ->
+    MindWayAndroidTheme { _, typography ->
         Column(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            HomeIcon(modifier = modifier, isSelected = isSelected)
+            icon()
             Spacer(modifier = modifier.height(4.dp))
             Text(
-                text = when (type) {
-                    HOME -> stringResource(id = R.string.home)
-                    EVENT -> stringResource(id = R.string.event)
-                    BOOKS -> stringResource(id = R.string.books)
-                    MY -> stringResource(id = R.string.my)
-                },
+                text = text,
                 style = typography.labelLarge,
                 fontWeight = FontWeight.Medium,
                 fontSize = 11.sp,
-                color = if (isSelected) colors.Black else colors.GRAY400
+                color = colors
             )
         }
     }
@@ -54,7 +45,8 @@ fun MindWayNavBarItem(
 @Composable
 fun MindWayNavBarItemPreview() {
     MindWayNavBarItem(
-        type = HOME,
-        isSelected = true
+        text = "홈",
+        colors = Color.Black,
+        icon = { HomeIcon() }
     )
 }
