317018021ff9993de33a3b9562ff8d0b53d18e1a
혜성 <118062596+answad@users.noreply.github.com>
2024-05-13 15:36:01 +0900

    :fire: :: ViewDetailTopAppBar 삭제

--- presentation/src/main/java/com/chobo/presentation/view/main/screen/ViewDetailScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/main/screen/ViewDetailScreen.kt
@@ -21,9 +21,12 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.R
 import com.chobo.presentation.view.component.bottom_sheet.MindWayBottomSheet
 import com.chobo.presentation.view.component.bottom_sheet.MindWayBottomSheetDialog
+import com.chobo.presentation.view.component.icon.ChevronLeftIcon
+import com.chobo.presentation.view.component.icon.OptionIcon
+import com.chobo.presentation.view.component.multipleEventsCutterManager.clickableSingle
+import com.chobo.presentation.view.component.topBar.MindWayTopAppBar
 import com.chobo.presentation.view.main.component.ViewDetailPopUp
 import com.chobo.presentation.view.main.component.ViewDetailTextCard
-import com.chobo.presentation.view.main.component.ViewDetailTopAppBar
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
 import com.chobo.presentation.viewModel.main.ViewDetailViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -94,9 +97,10 @@ internal fun ViewDetailScreen(
                         )
                     }
                 }
-                ViewDetailTopAppBar(
-                    startIconOnClick = navigateToBack,
-                    endIconOnClick = { coroutineScope.launch { sheetState.show() } }
+                MindWayTopAppBar(
+                    startIcon = { ChevronLeftIcon(modifier = Modifier.clickableSingle(onClick = navigateToBack)) },
+                    endIcon = { OptionIcon(modifier = Modifier.clickableSingle(onClick = { coroutineScope.launch { sheetState.show() } })) },
+                    midText = stringResource(R.string.view_detail),
                 )
                 Column(
                     verticalArrangement = Arrangement.spacedBy(20.dp, Alignment.Top),
