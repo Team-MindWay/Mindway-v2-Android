ceca48a650335017f6cc692d290e1e8423857d49
혜성 <118062596+answad@users.noreply.github.com>
2024-05-13 12:27:07 +0900

    :fire: :: DetailEventTopBar 삭제

--- presentation/src/main/java/com/chobo/presentation/view/event/screen/DetailEventScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/screen/DetailEventScreen.kt
@@ -13,10 +13,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.chobo.presentation.R
+import com.chobo.presentation.view.component.icon.ChevronLeftIcon
+import com.chobo.presentation.view.component.multipleEventsCutterManager.clickableSingle
+import com.chobo.presentation.view.component.topBar.MindWayTopAppBar
 import com.chobo.presentation.view.event.component.DetailEventContent
 import com.chobo.presentation.view.event.component.DetailEventTopBar
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
@@ -54,7 +59,10 @@ internal fun DetailEventScreen(
 ) {
     MindWayAndroidTheme { colors, _ ->
         Column(modifier = modifier.background(color = colors.WHITE)) {
-            DetailEventTopBar(startIconOnClick = navigateToBack)
+            MindWayTopAppBar(
+                startIcon = { ChevronLeftIcon(modifier = Modifier.clickableSingle(onClick = navigateToBack)) },
+                midText = stringResource(id = R.string.ongoing_event)
+            )
             Column(
                 modifier = Modifier
                     .fillMaxSize()
