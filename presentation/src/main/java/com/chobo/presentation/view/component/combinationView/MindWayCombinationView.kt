0358c7acd064bf540a1b4f050071d04683723f8c
혜성 <118062596+answad@users.noreply.github.com>
2024-05-13 12:23:31 +0900

    :fire: :: MyBottomSheet 삭제

--- presentation/src/main/java/com/chobo/presentation/view/component/combinationView/MindWayCombinationView.kt
+++ presentation/src/main/java/com/chobo/presentation/view/component/combinationView/MindWayCombinationView.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.chobo.presentation.R
 import com.chobo.presentation.view.book.screen.BookRoute
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBar
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType
@@ -18,10 +20,10 @@ import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBar
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.EVENT
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.HOME
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.MY
+import com.chobo.presentation.view.component.bottom_sheet.MindWayBottomSheet
 import com.chobo.presentation.view.component.bottom_sheet.MindWayBottomSheetDialog
 import com.chobo.presentation.view.event.screen.EventScreenRoute
 import com.chobo.presentation.view.main.screen.HomeRoute
-import com.chobo.presentation.view.my.component.MyBottomSheet
 import com.chobo.presentation.view.my.screen.MyRoute
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -40,9 +42,11 @@ fun MindWayCombinationView(
 ) {
     MindWayBottomSheetDialog(
         sheetContent = {
-            MyBottomSheet(
-                navigateToIntro = navigateToIntro,
-                logoutOnClick = navigateToLogin
+            MindWayBottomSheet(
+                topText = stringResource(R.string.mindway_intro),
+                bottomText = stringResource(R.string.logout),
+                topOnClick = navigateToIntro,
+                bottomOnCLick = navigateToLogin
             )
         }
     ) { sheetState ->
