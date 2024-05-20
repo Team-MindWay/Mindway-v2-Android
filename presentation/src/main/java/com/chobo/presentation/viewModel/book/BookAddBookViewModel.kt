fb140562ab2908a1c34a57a7c4216a2b5ffd9820
혜성 <118062596+answad@users.noreply.github.com>
2024-05-20 17:38:41 +0900

    :fire: :: 안쓰는 임포트 삭제

--- presentation/src/main/java/com/chobo/presentation/viewModel/book/BookAddBookViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/book/BookAddBookViewModel.kt
@@ -1,6 +1,5 @@
 package com.chobo.presentation.viewModel.book
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chobo.domain.model.order.OrderRequestBodyModel
