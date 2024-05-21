38a88d10485a17724f8eb8bccf2fb93f75b36c0c
혜성 <118062596+answad@users.noreply.github.com>
2024-05-21 10:11:14 +0900

    :sparkles: :: BookRoute 에서 viewModel 주입하는방법을 viewModel() -> hiltViewModel()으로 변경

--- presentation/src/main/java/com/chobo/presentation/view/book/screen/BookScreen.kt
+++ presentation/src/main/java/com/chobo/presentation/view/book/screen/BookScreen.kt
@@ -2,25 +2,43 @@
 
 package com.chobo.presentation.view.book.screen
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.*
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.pager.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.TabRow
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chobo.presentation.R
-import com.chobo.presentation.view.book.component.*
+import com.chobo.presentation.view.book.component.BookListItem
+import com.chobo.presentation.view.book.component.BookListItemData
+import com.chobo.presentation.view.book.component.BookTabRowItem
 import com.chobo.presentation.view.component.customToast.MindWayToast
 import com.chobo.presentation.view.component.icon.PlusIcon
 import com.chobo.presentation.view.component.multipleEventsCutterManager.clickableSingle
@@ -32,7 +50,7 @@ import kotlinx.coroutines.launch
 @Composable
 internal fun BookRoute(
     modifier: Modifier = Modifier,
-    bookScreenViewModel: BookScreenViewModel = viewModel(),
+    bookScreenViewModel: BookScreenViewModel = hiltViewModel(),
     navigateToBookAddBook: () -> Unit,
 ) {
     val novelDataList by bookScreenViewModel.novelDataList.collectAsStateWithLifecycle()
