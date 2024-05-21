fe701b23556da142fbe3f0e9802c0fd9270d63a2
혜성 <118062596+answad@users.noreply.github.com>
2024-05-21 20:19:27 +0900

    :sparkles: :: GetRecommendBookUiState에 Empty추가, Error -> Fail 이름 변경

--- presentation/src/main/java/com/chobo/presentation/viewModel/book/uistate/GetRecommendBookUiState.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/book/uistate/GetRecommendBookUiState.kt
@@ -4,6 +4,7 @@ import com.chobo.domain.model.recommend.response.RecommendListResponseAllModel
 
 sealed interface GetRecommendBookUiState {
     object Loading : GetRecommendBookUiState
+    object Empty: GetRecommendBookUiState
     data class Success(val data: List<RecommendListResponseAllModel>) : GetRecommendBookUiState
-    data class Error(val exception: Throwable) : GetRecommendBookUiState
+    data class Fail(val exception: Throwable): GetRecommendBookUiState
 }
