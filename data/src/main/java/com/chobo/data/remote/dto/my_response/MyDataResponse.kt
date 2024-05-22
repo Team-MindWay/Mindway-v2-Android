b0bd305a808ca02f7d725817471c6908a1895326
혜성 <118062596+answad@users.noreply.github.com>
2024-05-22 17:00:57 +0900

    :sparkles: :: MyData의 Model, Dto 추가

--- data/src/main/java/com/chobo/data/remote/dto/my_response/MyDataResponse.kt
+++ data/src/main/java/com/chobo/data/remote/dto/my_response/MyDataResponse.kt
@@ -1,6 +1,14 @@
 package com.chobo.data.remote.dto.my_response
 
+import com.chobo.domain.model.my.MyDataModel
+
 data class MyDataResponse(
     val name: String,
     val authority: String,
 )
+
+fun MyDataResponse.toModel() =
+    MyDataModel(
+        name = name,
+        authority = authority
+    )
