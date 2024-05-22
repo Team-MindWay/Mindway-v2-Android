e4fe45781038f24725fbaecadf6e868a6ab5ab87
혜성 <118062596+answad@users.noreply.github.com>
2024-05-22 17:01:25 +0900

    :sparkles: :: UseCase추가

---
+++ domain/src/main/java/com/chobo/domain/usecase/my/GetMyBookListUseCase.kt
@@ -0,0 +1,14 @@
+
+package com.chobo.domain.usecase.my
+
+import com.chobo.domain.model.my.MyBookListModel
+import com.chobo.domain.repository.MyRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetMyBookListUseCase @Inject constructor(
+    private val myRepository: MyRepository
+) {
+    suspend operator fun invoke(): Flow<MyBookListModel> =
+        myRepository.myBookListGet()
+}
