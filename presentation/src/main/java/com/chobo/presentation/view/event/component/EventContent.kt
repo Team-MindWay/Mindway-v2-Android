ee73dc017e0d16e1ccc0744213819f8af754020d
명훈 <s23065@gsm.hs.kr>
2024-05-27 21:20:51 +0900

    :memo: :: Change Data Type

--- presentation/src/main/java/com/chobo/presentation/view/event/component/EventContent.kt
+++ presentation/src/main/java/com/chobo/presentation/view/event/component/EventContent.kt
@@ -1,7 +1,12 @@
 package com.chobo.presentation.view.event.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Text
@@ -11,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.chobo.domain.model.event.response.GetEventListResponseModel
 import com.chobo.presentation.view.component.icon.BookImage
 import com.chobo.presentation.view.theme.MindWayAndroidTheme
 
@@ -18,7 +24,7 @@ import com.chobo.presentation.view.theme.MindWayAndroidTheme
 fun EventContent(
     modifier: Modifier = Modifier,
     content: String,
-    eventDataList: List<EventsData> = listOf(),
+    eventDataList: List<GetEventListResponseModel> = listOf(),
     eventDataListIsEmpty: Boolean,
     onIconClick: (Int) -> Unit,
     navigateToDetailEvent: () -> Unit,
