8159f6ac818088b610c22a3585493ec59df970fc
혜성 <118062596+answad@users.noreply.github.com>
2024-05-28 16:02:07 +0900

    :sparkles: :: bottomSheet의 변경사항에 맞춰서 변경

--- presentation/src/main/java/com/chobo/presentation/view/component/combinationView/MindWayCombinationView.kt
+++ presentation/src/main/java/com/chobo/presentation/view/component/combinationView/MindWayCombinationView.kt
@@ -1,10 +1,15 @@
 package com.chobo.presentation.view.component.combinationView
 
+import androidx.activity.ComponentActivity
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.chobo.presentation.R
 import com.chobo.presentation.view.book.screen.BookRoute
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBar
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType
@@ -12,13 +17,19 @@ import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBar
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.EVENT
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.HOME
 import com.chobo.presentation.view.component.bottom_navigation_bar.MindWayNavBarItemType.MY
+import com.chobo.presentation.view.component.bottom_sheet.MindWayBottomSheet
+import com.chobo.presentation.view.component.bottom_sheet.MindWayBottomSheetDialog
 import com.chobo.presentation.view.event.screen.EventScreenRoute
 import com.chobo.presentation.view.main.screen.HomeRoute
 import com.chobo.presentation.view.my.screen.MyRoute
+import com.chobo.presentation.viewModel.my.MyViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun MindWayCombinationView(
     topDestination: MutableState<MindWayNavBarItemType>,
+    myViewModel: MyViewModel = viewModel(LocalContext.current as ComponentActivity),
     navigateToDetailEvent: () -> Unit,
     navigateToGoalReading: () -> Unit,
     navigateToBookAddBook: () -> Unit,
@@ -26,37 +37,58 @@ fun MindWayCombinationView(
     navigateToMyBookEdit: () -> Unit,
     navigateToLogin: () -> Unit,
 ) {
-    Scaffold(
-        bottomBar = {
-            MindWayNavBar(
-                currentDestination = topDestination,
-                navigateToHome = { topDestination.value = HOME },
-                navigateToEvent = { topDestination.value = EVENT },
-                navigateToBooks = { topDestination.value = BOOKS },
-                navigateToMy = { topDestination.value = MY }
+    val coroutine = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(
+        ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = true
+    )
+
+    MindWayBottomSheetDialog(
+        sheetContent = {
+            MindWayBottomSheet(
+                topText = stringResource(R.string.mindway_intro),
+                bottomText = stringResource(R.string.logout),
+                topOnClick = navigateToIntro,
+                bottomOnCLick = {
+                    myViewModel.logout()
+                    navigateToLogin()
+                },
             )
-        }
-    ) { paddingValues ->
-        Box(modifier = Modifier.padding(paddingValues)) {
-            when (topDestination.value) {
-                HOME -> HomeRoute(
-                    navigateToGoalReading = navigateToGoalReading,
-                    navigateToDetailEvent = navigateToDetailEvent,
+        },
+        sheetState = sheetState
+    ) {
+        Scaffold(
+            bottomBar = {
+                MindWayNavBar(
+                    currentDestination = topDestination,
+                    navigateToHome = { topDestination.value = HOME },
+                    navigateToEvent = { topDestination.value = EVENT },
+                    navigateToBooks = { topDestination.value = BOOKS },
+                    navigateToMy = { topDestination.value = MY }
                 )
+            }
+        ) { paddingValues ->
+            Box(modifier = Modifier.padding(paddingValues)) {
+                when (topDestination.value) {
+                    HOME -> HomeRoute(
+                        navigateToGoalReading = navigateToGoalReading,
+                        navigateToDetailEvent = navigateToDetailEvent,
+                    )
 
-                EVENT -> EventScreenRoute(navigateToDetailEvent = navigateToDetailEvent)
+                    EVENT -> EventScreenRoute(navigateToDetailEvent = navigateToDetailEvent)
 
-                BOOKS -> BookRoute(navigateToBookAddBook = navigateToBookAddBook)
+                    BOOKS -> BookRoute(navigateToBookAddBook = navigateToBookAddBook)
 
-                MY -> MyRoute(
-                    navigateToIntro = navigateToIntro,
-                    navigateToLogin = navigateToLogin,
-                    navigateToMyBookEdit = navigateToMyBookEdit,
-                )
+                    MY -> MyRoute(
+                        showSheet = { coroutine.launch { sheetState.show() } },
+                        navigateToMyBookEdit = navigateToMyBookEdit,
+                    )
+                }
             }
         }
     }
 }
+
 
 @Preview
 @Composable
