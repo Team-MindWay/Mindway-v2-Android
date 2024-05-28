2e402e4820c743adb5b014213dff21302f2453be
명훈 <s23065@gsm.hs.kr>
2024-05-28 16:04:54 +0900

    :memo: :: Update DetailEventViewModel

--- presentation/src/main/java/com/chobo/presentation/viewModel/event/DetailEventViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/event/DetailEventViewModel.kt
@@ -2,35 +2,33 @@ package com.chobo.presentation.viewModel.event
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.chobo.domain.model.event.response.GetDetailEventResponseModel
-import com.chobo.presentation.view.util.decodeBase64ToBitmap
+import com.chobo.domain.usecase.event.GetDetailEventUseCase
+import com.chobo.presentation.viewModel.event.uistate.GetDetailEventUiState
+import com.chobo.presentation.viewModel.util.result.Result
+import com.chobo.presentation.viewModel.util.result.asResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DetailEventViewModel @Inject constructor() : ViewModel() {
-    private val _detailData = MutableStateFlow(GetDetailEventResponseModel())
-    val detailData: StateFlow<GetDetailEventResponseModel> = _detailData.asStateFlow()
+class DetailEventViewModel @Inject constructor(
+    private val getEventDetailUseCase: GetDetailEventUseCase,
+    ) : ViewModel() {
+    private val _getDetailEventUiState = MutableStateFlow<GetDetailEventUiState>(GetDetailEventUiState.Loading)
+    val getDetailEventUiState = _getDetailEventUiState.asStateFlow()
 
-    private suspend fun setDetailData(getDetailEventResponseModel: GetDetailEventResponseModel) {
-        _detailData.value = getDetailEventResponseModel
-    }
-
-    init {
-        viewModelScope.launch {
-            setDetailData(
-                GetDetailEventResponseModel(
-                    title = "가을 독서 행사",
-                    content = "독서의 계절, 가을을 맞아 도서관에서 특별한 이벤트를 준비했습니다. 랜덤으로 초성 책 제목이 적혀있는 쪽지를 뽑고, 그에 맞는 책을 찾아오면 푸짐한 선물뽑기를 할 수 있습니다. 점심시간마다 진행할 예정이니 많은 관심 바랍니다.",
-                    image = decodeBase64ToBitmap("dsdsadas").toString(),
-                    startedAt = "2023년 06월 20일",
-                    endedAt = "2023년 07월 08일"
-                )
-            )
-        }
+    fun getDetailEvent(eventId: Long) = viewModelScope.launch {
+        getEventDetailUseCase(eventId = eventId)
+            .asResult()
+            .collectLatest { result ->
+                when(result) {
+                    is Result.Loading -> _getDetailEventUiState.value = GetDetailEventUiState.Loading
+                    is Result.Success -> _getDetailEventUiState.value = GetDetailEventUiState.Success(result.data)
+                    is Result.Fail -> _getDetailEventUiState.value = GetDetailEventUiState.Fail(result.exception)
+                }
+            }
     }
 }
