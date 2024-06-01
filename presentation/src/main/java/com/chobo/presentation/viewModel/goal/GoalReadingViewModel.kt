0b855b23d8ec24d0e8cd62254df6b9839576fe92
혜성 <118062596+answad@users.noreply.github.com>
2024-06-01 22:21:37 +0900

    :sparkles: :: getWeekendGoalUseCase 를 이용해서 getWeekendGoal 함수 생성

--- presentation/src/main/java/com/chobo/presentation/viewModel/goal/GoalReadingViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/goal/GoalReadingViewModel.kt
@@ -1,7 +1,11 @@
 package com.chobo.presentation.viewModel.goal
 
 import androidx.lifecycle.*
+import com.chobo.domain.usecase.goal.GetWeekendGoalUseCase
 import com.chobo.presentation.view.main.component.*
+import com.chobo.presentation.viewModel.main.uistate.GetWeekendGoalUiState
+import com.chobo.presentation.viewModel.util.result.Result
+import com.chobo.presentation.viewModel.util.result.asResult
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
@@ -9,21 +13,17 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GoalReadingViewModel @Inject constructor() : ViewModel() {
-    private val _goalBookRead = MutableStateFlow(0)
-    val goalBookRead: StateFlow<Int> = _goalBookRead.asStateFlow()
-
-    private val _goalBookReadIsEmpty = MutableStateFlow(_goalBookRead.value == 0)
-    val goalBookReadIsEmpty: StateFlow<Boolean> = _goalBookReadIsEmpty.asStateFlow()
+class GoalReadingViewModel @Inject constructor(
+    private val getWeekendGoalUseCase: GetWeekendGoalUseCase
+) : ViewModel() {
+    private val _getWeekendGoalUiState = MutableStateFlow<GetWeekendGoalUiState>(GetWeekendGoalUiState.Loading)
+    val getWeekendGoalUiState: StateFlow<GetWeekendGoalUiState> = _getWeekendGoalUiState.asStateFlow()
 
     private val _goalBookReadSetting = MutableStateFlow("")
     val goalBookReadSetting: StateFlow<String> = _goalBookReadSetting.asStateFlow()
 
     private val _goalBookReadSettingIsEmpty = MutableStateFlow(false)
     val goalBookReadSettingIsEmpty: StateFlow<Boolean> = _goalBookReadSettingIsEmpty.asStateFlow()
-
-    private val _goalReadingGraphDataList = MutableStateFlow<List<GoalReadingGraphData>>(listOf())
-    val goalReadingGraphDataList: StateFlow<List<GoalReadingGraphData>> = _goalReadingGraphDataList.asStateFlow()
 
     private val _goalReadingListOfBooksReadItemDataList = MutableStateFlow<List<GoalReadingListOfBooksReadItemData>>(listOf())
     val goalReadingListOfBooksReadItemDataList: StateFlow<List<GoalReadingListOfBooksReadItemData>> = _goalReadingListOfBooksReadItemDataList.asStateFlow()
@@ -34,6 +34,22 @@ class GoalReadingViewModel @Inject constructor() : ViewModel() {
     private val _isSuccess = MutableStateFlow(false)
     val isSuccess: StateFlow<Boolean> = _isSuccess.asStateFlow()
 
+    fun getWeekendGoal() = viewModelScope.launch {
+        getWeekendGoalUseCase()
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is Result.Loading -> _getWeekendGoalUiState.value = GetWeekendGoalUiState.Loading
+                    is Result.Success -> if (result.data.now_count + result.data.goal_value == 0) {
+                        _getWeekendGoalUiState.value = GetWeekendGoalUiState.Empty
+                    } else {
+                        _getWeekendGoalUiState.value = GetWeekendGoalUiState.Success(result.data)
+                    }
+
+                    is Result.Fail -> _getWeekendGoalUiState.value = GetWeekendGoalUiState.Fail(result.exception)
+                }
+            }
+    }
     fun updateGoalBookReadSetting(input: String) {
         _goalBookReadSettingIsEmpty.value = false
         _goalBookReadSetting.value = input
