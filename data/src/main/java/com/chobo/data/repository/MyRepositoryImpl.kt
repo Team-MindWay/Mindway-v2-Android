716a2f63023340f4d8c15e35ded21e4bde513544
혜성 <118062596+answad@users.noreply.github.com>
2024-06-01 15:37:05 +0900

    :sparkles: :: myBookListGet API의 반환값 List<MyBookListResponse>으로 변경

--- data/src/main/java/com/chobo/data/repository/MyRepositoryImpl.kt
+++ data/src/main/java/com/chobo/data/repository/MyRepositoryImpl.kt
@@ -10,14 +10,13 @@ import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class MyRepositoryImpl @Inject constructor(
-    private val myDataSource: RemoteMyDataSource
+    private val myDataSource: RemoteMyDataSource,
 ) : MyRepository {
     override suspend fun myInformationGet(): Flow<MyDataModel> {
         return myDataSource.getMyInformation().map { it.toModel() }
     }
 
-    override suspend fun myBookListGet(): Flow<MyBookListModel> {
-        return myDataSource.getMyBookList().map { it.toModel() }
+    override suspend fun myBookListGet(): Flow<List<MyBookListModel>> {
+        return myDataSource.getMyBookList().map { list -> list.map { it.toModel() } }
     }
-
 }
