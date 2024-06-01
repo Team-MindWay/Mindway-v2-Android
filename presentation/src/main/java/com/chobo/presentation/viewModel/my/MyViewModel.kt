02201d053479db7d5e15c7fabcee7f8efd29e6a9
혜성 <118062596+answad@users.noreply.github.com>
2024-06-01 16:08:04 +0900

    :sparkles: :: OrderDeleteByIdUseCase 와 orderDeleteById(MyViewModel) 연결

--- presentation/src/main/java/com/chobo/presentation/viewModel/my/MyViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/my/MyViewModel.kt
@@ -7,6 +7,7 @@ import com.chobo.domain.usecase.auth.DeleteTokenUseCase
 import com.chobo.domain.usecase.auth.LogoutUseCase
 import com.chobo.domain.usecase.my.GetMyBookListUseCase
 import com.chobo.domain.usecase.my.GetMyInformationUseCase
+import com.chobo.domain.usecase.order.OrderDeleteByIdUseCase
 import com.chobo.presentation.viewModel.my.UiState.GetMyBookListUiState
 import com.chobo.presentation.viewModel.my.UiState.GetMyInformationUiState
 import com.chobo.presentation.viewModel.util.result.Result
@@ -26,6 +27,7 @@ class MyViewModel @Inject constructor(
     private val deleteTokenUseCase: DeleteTokenUseCase,
     private val getMyInformationUseCase: GetMyInformationUseCase,
     private val getMyBookListUseCase: GetMyBookListUseCase,
+    private val orderDeleteByIdUseCase: OrderDeleteByIdUseCase,
 ) : ViewModel() {
     private val _getMyBookListUiState = MutableStateFlow<GetMyBookListUiState>(GetMyBookListUiState.Loading)
     val getMyBookListUiState: StateFlow<GetMyBookListUiState> = _getMyBookListUiState.asStateFlow()
@@ -38,6 +40,9 @@ class MyViewModel @Inject constructor(
 
     private val _isToastVisible = MutableStateFlow(false)
     val isToastVisible: StateFlow<Boolean> = _isToastVisible.asStateFlow()
+
+    private val _isCommunicationSuccess = MutableStateFlow(false)
+    val isCommunicationSuccess: StateFlow<Boolean> = _isCommunicationSuccess.asStateFlow()
 
     private val _bookDeleteDialogIsVisible = MutableStateFlow(false)
     val bookDeleteDialogIsVisible: StateFlow<Boolean> = _bookDeleteDialogIsVisible.asStateFlow()
@@ -80,11 +85,26 @@ class MyViewModel @Inject constructor(
                         _getMyBookListUiState.value = GetMyBookListUiState.Success(result.data)
                     }
 
-                    is Result.Fail -> _getMyBookListUiState.value = GetMyBookListUiState.Fail(result.exception)
+                    is Result.Fail -> _getMyBookListUiState.value =
+                        GetMyBookListUiState.Fail(result.exception)
                 }
             }
     }
 
+    fun orderDeleteById(id: Long) = viewModelScope.launch {
+        orderDeleteByIdUseCase(orderId = id)
+            .asResult()
+            .collectLatest { result ->
+                when (result) {
+                    is Result.Loading -> _isCommunicationSuccess.value = false
+
+                    is Result.Success -> _isCommunicationSuccess.value = true
+
+                    is Result.Fail -> _isCommunicationSuccess.value = false
+                }
+            }
+        showToast()
+    }
 
     fun toggleBookDeleteDialogIsVisible() {
         _bookDeleteDialogIsVisible.value = !_bookDeleteDialogIsVisible.value
@@ -106,6 +126,7 @@ class MyViewModel @Inject constructor(
 
     init {
         getMyInformation()
+        orderDeleteById(id = 0)
         _getMyBookListUiState.value = GetMyBookListUiState.Success(
             data = MutableList(10) {
                 MyBookListModel(
