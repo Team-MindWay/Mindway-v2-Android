ef3e526b2a49717b3b380ff0244a9e924fbb5b77
혜성 <118062596+answad@users.noreply.github.com>
2024-06-02 09:58:58 +0900

    :sparkles: :: bookListGet APi에서 각각의 아이템에 대한 id를 받도록 변경

--- data/src/main/java/com/chobo/data/remote/dto/book/response/BookListResponse.kt
+++ data/src/main/java/com/chobo/data/remote/dto/book/response/BookListResponse.kt
@@ -1,14 +1,17 @@
 package com.chobo.data.remote.dto.book.response
 
 import com.chobo.domain.model.book.response.BookListResponseModel
+import java.time.LocalDateTime
 
 data class BookListResponse(
+    val id: Long,
     val title: String,
     val plot: String,
-    val created_at: String
+    val created_at: LocalDateTime,
 )
 
 fun BookListResponse.toModel() = BookListResponseModel(
+    id = id,
     title = title,
     plot = plot,
     created_at = created_at
--- domain/src/main/java/com/chobo/domain/model/book/response/BookListResponseModel.kt
+++ domain/src/main/java/com/chobo/domain/model/book/response/BookListResponseModel.kt
@@ -1,7 +1,10 @@
 package com.chobo.domain.model.book.response
 
+import java.time.LocalDateTime
+
 data class BookListResponseModel(
+    val id: Long,
     val title: String,
     val plot: String,
-    val created_at: String
+    val created_at: LocalDateTime,
 )
