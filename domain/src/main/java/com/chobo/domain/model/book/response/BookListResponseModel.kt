150cfbd3fe6f36e1668b88011905434b78c76427
혜성 <118062596+answad@users.noreply.github.com>
2024-06-03 15:38:08 +0900

    created_at 이름을 date로 바꾸고, 타입을 LocalDateTime 에서 String 으로 바꿨습니다

--- domain/src/main/java/com/chobo/domain/model/book/response/BookListResponseModel.kt
+++ domain/src/main/java/com/chobo/domain/model/book/response/BookListResponseModel.kt
@@ -1,10 +1,8 @@
 package com.chobo.domain.model.book.response
 
-import java.time.LocalDateTime
-
 data class BookListResponseModel(
     val id: Long,
     val title: String,
     val plot: String,
-    val created_at: LocalDateTime,
+    val date: String,
 )
