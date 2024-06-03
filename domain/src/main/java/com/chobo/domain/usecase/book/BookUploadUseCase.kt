397c9350f39d0933f6b1b6a5540d1ef7fadc0439
혜성 <118062596+answad@users.noreply.github.com>
2024-06-03 15:43:44 +0900

    BookUploadUseCase의 반환타입을 Flow 로 변경

--- domain/src/main/java/com/chobo/domain/usecase/book/BookUploadUseCase.kt
+++ domain/src/main/java/com/chobo/domain/usecase/book/BookUploadUseCase.kt
@@ -2,12 +2,12 @@ package com.chobo.domain.usecase.book
 
 import com.chobo.domain.model.book.request.BookRequestBodyModel
 import com.chobo.domain.repository.BookRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class BookUploadUseCase @Inject constructor(
-    private val bookRepository: BookRepository
+    private val bookRepository: BookRepository,
 ) {
-    suspend operator fun invoke(body: BookRequestBodyModel) = runCatching {
+    suspend operator fun invoke(body: BookRequestBodyModel): Flow<Unit> =
         bookRepository.bookUpload(body = body)
-    }
 }
