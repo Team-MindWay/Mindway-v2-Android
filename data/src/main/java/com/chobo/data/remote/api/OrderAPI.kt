e17a784d5c8fc642c3a459246a19784974c50daf
명훈 <s23065@gsm.hs.kr>
2024-06-05 08:04:05 +0900

    :fire: :: Fire Unused Code(orderAPI -> GET, DELETE)

--- data/src/main/java/com/chobo/data/remote/api/OrderAPI.kt
+++ data/src/main/java/com/chobo/data/remote/api/OrderAPI.kt
@@ -1,7 +1,10 @@
 package com.chobo.data.remote.api
 
 import com.chobo.data.remote.dto.order_request.OrderRequestBody
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface OrderAPI {
     @POST("/api/v2/order")
@@ -9,17 +12,9 @@ interface OrderAPI {
         @Body body: OrderRequestBody
     )
 
-    @GET("/api/v2/order")
-    suspend fun orderListGet(): List<OrderRequestBody>
-
     @PATCH("/api/v2/order/{order_id}")
     suspend fun orderModifyById(
         @Body body: OrderRequestBody,
         @Path("order_id") orderId: Long,
-    )
-
-    @DELETE("/api/v2/order/{order_id}")
-    suspend fun orderDeleteById(
-        @Path("order_id") orderId: String,
     )
 }
