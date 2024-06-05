bd943638d55184ad50c593144802a94ee0a1efcc
명훈 <s23065@gsm.hs.kr>
2024-06-05 09:55:37 +0900

    :truck: :: Changed File Name(GetEventListUiState->GetNowEventListState)

--- presentation/src/main/java/com/chobo/presentation/viewModel/event/uistate/GetNowEventListUiState.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/event/uistate/GetNowEventListUiState.kt
@@ -2,9 +2,9 @@ package com.chobo.presentation.viewModel.event.uistate
 
 import com.chobo.domain.model.event.response.GetEventListResponseModel
 
-sealed interface GetEventListUiState {
-    object Loading: GetEventListUiState
-    object Empty: GetEventListUiState
-    data class Success(val getEventListResponse: List<GetEventListResponseModel>): GetEventListUiState
-    data class Fail(val exception: Throwable): GetEventListUiState
+sealed interface GetNowEventListUiState {
+    object Loading: GetNowEventListUiState
+    object Empty: GetNowEventListUiState
+    data class Success(val getEventListResponse: List<GetEventListResponseModel>): GetNowEventListUiState
+    data class Fail(val exception: Throwable): GetNowEventListUiState
 }
