4a0785137695b3233ad7713e783b94df7344a562
혜성 <118062596+answad@users.noreply.github.com>
2024-06-05 21:17:41 +0900

    MyBookListRespons와 MyBookListModel에 id 값 추가

--- data/src/main/java/com/chobo/data/remote/dto/my_response/MyBookListResponse.kt
+++ data/src/main/java/com/chobo/data/remote/dto/my_response/MyBookListResponse.kt
@@ -3,11 +3,13 @@ package com.chobo.data.remote.dto.my_response
 import com.chobo.domain.model.my.MyBookListModel
 
 data class MyBookListResponse(
+    val id: Long,
     val name: String,
     val author: String,
 )
 
 fun MyBookListResponse.toModel() = MyBookListModel(
+    id = id,
     name = name,
     author = author
 )
--- domain/src/main/java/com/chobo/domain/model/my/MyBookListModel.kt
+++ domain/src/main/java/com/chobo/domain/model/my/MyBookListModel.kt
@@ -1,6 +1,7 @@
 package com.chobo.domain.model.my
 
 data class MyBookListModel(
+    val id: Long,
     val name: String,
     val author: String,
 )
