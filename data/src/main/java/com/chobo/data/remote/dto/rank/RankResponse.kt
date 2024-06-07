3e799af92b01b72173eb21ea2a9667f53f16b8ee
혜성 <118062596+answad@users.noreply.github.com>
2024-06-07 16:51:29 +0900

    :bug: :: RankResponse과 RankModel의 username -> name , total -> accrue 변경

--- data/src/main/java/com/chobo/data/remote/dto/rank/RankResponse.kt
+++ data/src/main/java/com/chobo/data/remote/dto/rank/RankResponse.kt
@@ -3,11 +3,11 @@ package com.chobo.data.remote.dto.rank
 import com.chobo.domain.model.rank.RankModel
 
 data class RankResponse(
-    val username: String,
-    val total: Int
+    val name: String,
+    val accrue: Int
 )
 
 fun RankResponse.toModel() = RankModel(
-    username = username,
-    total = total,
+    name = name,
+    accrue = accrue,
 )
