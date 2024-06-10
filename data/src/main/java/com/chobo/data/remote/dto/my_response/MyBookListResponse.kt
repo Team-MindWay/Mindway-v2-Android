8afd9e98c2783a02939c652d8e73b74ece2bfef5
혜성 <118062596+answad@users.noreply.github.com>
2024-06-10 10:30:51 +0900

    :sparkles: :: MyBookListResponse에 bookUrl 추가

--- data/src/main/java/com/chobo/data/remote/dto/my_response/MyBookListResponse.kt
+++ data/src/main/java/com/chobo/data/remote/dto/my_response/MyBookListResponse.kt
@@ -1,15 +1,19 @@
 package com.chobo.data.remote.dto.my_response
 
 import com.chobo.domain.model.my.MyBookListModel
+import com.google.gson.annotations.SerializedName
 
 data class MyBookListResponse(
     val id: Long,
     val title: String,
     val author: String,
+    @SerializedName("book_url")
+    val bookUrl: String
 )
 
 fun MyBookListResponse.toModel() = MyBookListModel(
     id = id,
     title = title,
-    author = author
+    author = author,
+    bookUrl = bookUrl
 )
--- domain/src/main/java/com/chobo/domain/model/my/MyBookListModel.kt
+++ domain/src/main/java/com/chobo/domain/model/my/MyBookListModel.kt
@@ -4,4 +4,5 @@ data class MyBookListModel(
     val id: Long,
     val title: String,
     val author: String,
+    val bookUrl: String
 )
