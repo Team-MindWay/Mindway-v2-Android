09160f7d38539654c6ffdff16ac45f5002f949f2
명훈 <s23065@gsm.hs.kr>
2024-06-13 18:16:38 +0900

    :memo: :: Apply performApiRequest

--- data/src/main/java/com/chobo/data/remote/datasource/event/RemoteEventDataSourceImpl.kt
+++ data/src/main/java/com/chobo/data/remote/datasource/event/RemoteEventDataSourceImpl.kt
@@ -3,37 +3,19 @@ package com.chobo.data.remote.datasource.event
 import com.chobo.data.remote.api.EventAPI
 import com.chobo.data.remote.dto.event.response.GetDetailEventResponse
 import com.chobo.data.remote.dto.event.response.GetEventListResponse
-import com.chobo.data.util.MindWayAPIHandler
-import kotlinx.coroutines.Dispatchers
+import com.chobo.data.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemoteEventDataSourceImpl @Inject constructor(
     private val eventService: EventAPI
 ): RemoteEventDataSource {
     override suspend fun getEventList(status: String): Flow<List<GetEventListResponse>> = flow {
-        emit(
-            MindWayAPIHandler<List<GetEventListResponse>>()
-                .httpRequest {
-                    eventService.getEventList(
-                        status = status
-                    )
-                }
-                .sendRequest()
-        )
-    }.flowOn(Dispatchers.IO)
+        performApiRequest { eventService.getEventList(status = status) }
+    }
 
     override suspend fun getDetailEvent(eventId: Long): Flow<GetDetailEventResponse> = flow {
-        emit(
-            MindWayAPIHandler<GetDetailEventResponse>()
-                .httpRequest {
-                    eventService.getDetailEvent(
-                        eventId = eventId
-                    )
-                }
-                .sendRequest()
-        )
-    }.flowOn(Dispatchers.IO)
+        performApiRequest { eventService.getDetailEvent(eventId = eventId) }
+    }
 }
--- data/src/main/java/com/chobo/data/remote/datasource/my/RemoteMyDataSourceImpl.kt
+++ data/src/main/java/com/chobo/data/remote/datasource/my/RemoteMyDataSourceImpl.kt
@@ -3,33 +3,19 @@ package com.chobo.data.remote.datasource.my
 import com.chobo.data.remote.api.MyAPI
 import com.chobo.data.remote.dto.my_response.MyBookListResponse
 import com.chobo.data.remote.dto.my_response.MyDataResponse
-import com.chobo.data.util.MindWayAPIHandler
-import kotlinx.coroutines.Dispatchers
+import com.chobo.data.util.performApiRequest
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class RemoteMyDataSourceImpl @Inject constructor(
-    private val myAPI: MyAPI,
+    private val myService: MyAPI,
 ) : RemoteMyDataSource {
     override suspend fun getMyInformation(): Flow<MyDataResponse> = flow {
-        emit(
-            MindWayAPIHandler<MyDataResponse>()
-                .httpRequest {
-                    myAPI.myInformationGet()
-                }
-                .sendRequest()
-        )
-    }.flowOn(Dispatchers.IO)
+        performApiRequest { myService.myInformationGet() }
+    }
 
     override suspend fun getMyBookList(): Flow<List<MyBookListResponse>> = flow {
-        emit(
-            MindWayAPIHandler<List<MyBookListResponse>>()
-                .httpRequest {
-                    myAPI.myBookListGet()
-                }
-                .sendRequest()
-        )
-    }.flowOn(Dispatchers.IO)
+        performApiRequest { myService.myBookListGet() }
+    }
 }
