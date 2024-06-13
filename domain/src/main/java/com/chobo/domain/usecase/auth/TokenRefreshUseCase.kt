84596b565f6ecaaf0d751ce7555df817516159c1
명훈 <s23065@gsm.hs.kr>
2024-06-13 22:57:25 +0900

    :sparkles: :: Create TokenRefreshUseCase

---
+++ domain/src/main/java/com/chobo/domain/usecase/auth/TokenRefreshUseCase.kt
@@ -0,0 +1,12 @@
+package com.chobo.domain.usecase.auth
+
+import com.chobo.domain.repository.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class TokenRefreshUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(): Flow<String> =
+        authRepository.getGAuthAccess()
+}
