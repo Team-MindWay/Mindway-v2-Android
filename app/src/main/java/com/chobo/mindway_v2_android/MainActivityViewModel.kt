a5e34bc54fd539ee28b24358a8827d0de509d7ef
youloveli <mymhj122@gmail.com>
2024-07-25 08:33:50 +0900

    fix :: ci fix tokenRefreshUseCase값없음 문제

--- app/src/main/java/com/chobo/mindway_v2_android/MainActivityViewModel.kt
+++ app/src/main/java/com/chobo/mindway_v2_android/MainActivityViewModel.kt
@@ -1,10 +1,10 @@
 package com.chobo.mindway_v2_android
 
-import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.chobo.data.local.datasource.LocalAuthDataSource
 import com.chobo.domain.model.auth.response.GAuthLoginResponseModel
 import com.chobo.domain.usecase.auth.SaveLoginDataUseCase
 import com.chobo.domain.usecase.auth.TokenRefreshUseCase
@@ -19,6 +19,7 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(
     private val saveTokenUseCase: SaveLoginDataUseCase,
     private val tokenRefreshUseCase: TokenRefreshUseCase,
+    private val localAuthDataSource: LocalAuthDataSource
 ) : ViewModel() {
 
     var uiState: MutableState<MainActivityUiState> = mutableStateOf(MainActivityUiState.Loading)
@@ -27,26 +28,25 @@ class MainActivityViewModel @Inject constructor(
         tokenRefresh()
     }
 
-    private fun saveLoginToken(data: GAuthLoginResponseModel) = viewModelScope.launch {
-        saveTokenUseCase(data = data)
-        Log.d("saveLoginToken", data.toString())
-    }
-
     private fun tokenRefresh() = viewModelScope.launch {
-        tokenRefreshUseCase()
-            .asResult()
-            .collectLatest { result ->
-                when (result) {
-                    is Result.Fail -> uiState.value = MainActivityUiState.Fail(result.exception)
-                    is Result.Loading -> uiState.value = MainActivityUiState.Loading
-                    is Result.Success -> {
-                        saveLoginToken(result.data)
-                        uiState.value = MainActivityUiState.Success(result.data)
+        localAuthDataSource.getRefreshToken()
+            .collectLatest { refreshToken ->
+                tokenRefreshUseCase(refreshToken)
+                    .asResult()
+                    .collectLatest { result ->
+                        when (result) {
+                            is Result.Fail -> uiState.value = MainActivityUiState.Fail(result.exception)
+                            is Result.Loading -> uiState.value = MainActivityUiState.Loading
+                            is Result.Success -> {
+                                saveTokenUseCase(data = result.data)
+                                uiState.value = MainActivityUiState.Success(result.data)
+                            }
+                        }
                     }
-                }
             }
     }
 }
+
 
 sealed interface MainActivityUiState {
     object Loading : MainActivityUiState
