19721c76beeda7170b216fda9ac32fcc747693db
youloveli <mymhj122@gmail.com>
2024-07-25 13:29:48 +0900

    refactor :: LocalDataSourceModule을 DataStoreModule에 병합

--- app/src/main/java/com/chobo/mindway_v2_android/module/DataStoreModule.kt
+++ app/src/main/java/com/chobo/mindway_v2_android/module/DataStoreModule.kt
@@ -1,10 +1,14 @@
 package com.chobo.mindway_v2_android.module
 
 import android.content.Context
+import androidx.datastore.core.DataStore
 import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.chobo.data.local.datasource.LocalAuthDataSource
+import com.chobo.data.local.datasource.LocalAuthDataSourceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,11 +21,21 @@ import javax.inject.Singleton
 object DataStoreModule {
     @Provides
     @Singleton
-    fun providePreferencesDataStore(@ApplicationContext context: Context) =
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
         PreferenceDataStoreFactory.create(
             corruptionHandler = ReplaceFileCorruptionHandler(
                 produceNewData = { emptyPreferences() }
             ),
             produceFile = { context.preferencesDataStoreFile("autoDataStore") }
         )
+
+    @Provides
+    @Singleton
+    fun provideLocalAuthDataSource(
+        dataStore: DataStore<Preferences>
+    ): LocalAuthDataSource {
+        return LocalAuthDataSourceImpl(
+            dataStore = dataStore
+        )
+    }
 }
