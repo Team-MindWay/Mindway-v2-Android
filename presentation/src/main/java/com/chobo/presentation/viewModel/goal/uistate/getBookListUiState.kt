ccc5371dd14089ae160e197c19cc676eccdb438b
youloveli <mymhj122@gmail.com>
2024-08-08 18:37:51 +0900

    :recycle: :: uiState의 list들의 타입을 immutableState로 변경

--- presentation/src/main/java/com/chobo/presentation/viewModel/goal/uistate/getBookListUiState.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/goal/uistate/getBookListUiState.kt
@@ -1,10 +1,11 @@
 package com.chobo.presentation.viewModel.goal.uistate
 
 import com.chobo.domain.model.book.response.BookListResponseModel
+import kotlinx.collections.immutable.ImmutableList
 
 sealed interface GetBookListUiState {
     object Loading : GetBookListUiState
     object Empty : GetBookListUiState
-    data class Success(val data: List<BookListResponseModel>) : GetBookListUiState
+    data class Success(val data: ImmutableList<BookListResponseModel>) : GetBookListUiState
     data class Fail(val exception: Throwable) : GetBookListUiState
 }
--- presentation/src/main/java/com/chobo/presentation/viewModel/main/uistate/GetRankUiState.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/main/uistate/GetRankUiState.kt
@@ -1,10 +1,11 @@
 package com.chobo.presentation.viewModel.main.uistate
 
 import com.chobo.domain.model.rank.RankModel
+import kotlinx.collections.immutable.ImmutableList
 
 sealed interface GetRankUiState {
     object Loading : GetRankUiState
     object Empty : GetRankUiState
-    data class Success(val data: List<RankModel>) : GetRankUiState
+    data class Success(val data: ImmutableList<RankModel>) : GetRankUiState
     data class Fail(val exception: Throwable) : GetRankUiState
 }
