77a0da1f95c3631cfcc75f33012f197ce47fccf3
명훈 <s23065@gsm.hs.kr>
2024-08-26 15:28:34 +0900

    :lipstick: :: Add HomeErrorNoticeCard

---
+++ presentation/src/main/java/com/chobo/presentation/view/main/component/HomeErrorNoticeCard.kt
@@ -0,0 +1,57 @@
+package com.chobo.presentation.view.main.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chobo.presentation.view.theme.MindWayAndroidTheme
+
+@Composable
+fun HomeErrorNoticeCard(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    MindWayAndroidTheme { colors, typography ->
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .fillMaxWidth()
+                .shadow(
+                    elevation = 20.dp,
+                    spotColor = colors.CardShadow,
+                    ambientColor = colors.CardShadow
+                )
+                .background(
+                    color = colors.WHITE,
+                    shape = RoundedCornerShape(size = 8.dp)
+                )
+                .padding(horizontal = 12.dp)) {
+            Text(
+                modifier = modifier.padding(vertical = 36.5.dp),
+                text = text,
+                style = typography.bodySmall,
+                fontWeight = FontWeight.SemiBold,
+                color = colors.GRAY400,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun Pre() {
+    HomeErrorNoticeCard(
+        text = "통신 원활 (x)"
+    )
+}
