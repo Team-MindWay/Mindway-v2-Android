ec60a2247716323fe26e0172d5d8a273f5798497
명훈 <s23065@gsm.hs.kr>
2024-09-02 20:32:35 +0900

    :recycle: :: Use Moshi To my Module

--- data/src/main/java/com/chobo/data/remote/dto/my_response/MyBookListResponse.kt
+++ data/src/main/java/com/chobo/data/remote/dto/my_response/MyBookListResponse.kt
@@ -1,14 +1,15 @@
 package com.chobo.data.remote.dto.my_response
 
 import com.chobo.domain.model.my.MyBookListModel
-import com.google.gson.annotations.SerializedName
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class MyBookListResponse(
-    val id: Long,
-    val title: String,
-    val author: String,
-    @SerializedName("book_url")
-    val bookUrl: String
+    @Json(name = "id") val id: Long,
+    @Json(name = "title") val title: String,
+    @Json(name = "author") val author: String,
+    @Json(name = "book_url") val bookUrl: String
 )
 
 fun MyBookListResponse.toModel() = MyBookListModel(
