23f77a7884552f31b93fb64f15d2b5570a6243dd
명훈 <s23065@gsm.hs.kr>
2024-09-24 20:44:41 +0900

    🔥 :: Fire Unnecessary Annotation - recommend

--- domain/src/main/java/com/chobo/domain/model/recommend/request/RecommendRequestAllModel.kt
+++ domain/src/main/java/com/chobo/domain/model/recommend/request/RecommendRequestAllModel.kt
@@ -1,8 +1,5 @@
 package com.chobo.domain.model.recommend.request
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 data class RecommendRequestAllModel(
     val title: String,
     val content: String,
