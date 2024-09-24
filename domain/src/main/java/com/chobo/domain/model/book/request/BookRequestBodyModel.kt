854efca9ef768b3c181548a850fd8c5b5b93d355
명훈 <s23065@gsm.hs.kr>
2024-09-24 20:38:32 +0900

    🔥 :: Fire Unnecessary Annotation - book

--- domain/src/main/java/com/chobo/domain/model/book/request/BookRequestBodyModel.kt
+++ domain/src/main/java/com/chobo/domain/model/book/request/BookRequestBodyModel.kt
@@ -1,8 +1,5 @@
 package com.chobo.domain.model.book.request
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 data class BookRequestBodyModel(
     val title: String,
     val plot: String
