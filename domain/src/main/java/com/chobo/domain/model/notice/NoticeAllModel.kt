0c4beb3c8e1ab28863160637ba708c794dd2dc16
명훈 <s23065@gsm.hs.kr>
2024-09-24 20:42:57 +0900

    🔥 :: Fire Unnecessary Annotation - notice

--- domain/src/main/java/com/chobo/domain/model/notice/NoticeAllModel.kt
+++ domain/src/main/java/com/chobo/domain/model/notice/NoticeAllModel.kt
@@ -1,8 +1,5 @@
 package com.chobo.domain.model.notice
 
-import androidx.compose.runtime.Immutable
-
-@Immutable
 data class NoticeAllModel(
     val title: String = "",
     val content: String = ""
