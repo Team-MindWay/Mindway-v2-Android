ad3779c9830a9507f34cd7136af1f5360f65024b
명훈 <s23065@gsm.hs.kr>
2024-10-15 15:38:40 +0900

    :recycle: :: Add SwipeRefreshLoading Code -> StateFlow

--- presentation/src/main/java/com/chobo/presentation/viewModel/event/EventViewModel.kt
+++ presentation/src/main/java/com/chobo/presentation/viewModel/event/EventViewModel.kt
@@ -20,13 +20,19 @@ import javax.inject.Inject
 class EventViewModel @Inject constructor(
     private val getEventListUseCase: GetEventListUseCase
 ) : ViewModel() {
-    private val _getNowEventListUiState = MutableStateFlow<GetEventListUiState>(GetEventListUiState.Loading)
+    private val _swipeRefreshLoading = MutableStateFlow(false)
+    val swipeRefreshLoading = _swipeRefreshLoading.asStateFlow()
+
+    private val _getNowEventListUiState =
+        MutableStateFlow<GetEventListUiState>(GetEventListUiState.Loading)
     val getNowEventListUiState = _getNowEventListUiState.asStateFlow()
 
-    private val _getEventListUiState = MutableStateFlow<GetEventListUiState>(GetEventListUiState.Loading)
+    private val _getEventListUiState =
+        MutableStateFlow<GetEventListUiState>(GetEventListUiState.Loading)
     val getPastEventListUiState = _getEventListUiState.asStateFlow()
 
     fun getEventList(type: EventRequestListStatusType) = viewModelScope.launch {
+        _swipeRefreshLoading.value = true
         val targetData = when (type) {
             NOW -> _getNowEventListUiState
             PAST -> _getEventListUiState
@@ -38,12 +44,17 @@ class EventViewModel @Inject constructor(
                     is Result.Loading -> targetData.value = GetEventListUiState.Loading
                     is Result.Success -> if (result.data.isEmpty()) {
                         targetData.value = GetEventListUiState.Empty
+                        _swipeRefreshLoading.value = false
                     } else {
-                        targetData.value = GetEventListUiState.Success(result.data.toImmutableList())
+                        targetData.value =
+                            GetEventListUiState.Success(result.data.toImmutableList())
+                        _swipeRefreshLoading.value = false
                     }
 
-                    is Result.Fail -> targetData.value =
-                        GetEventListUiState.Fail
+                    is Result.Fail -> {
+                        targetData.value = GetEventListUiState.Fail
+                        _swipeRefreshLoading.value = false
+                    }
                 }
             }
     }
