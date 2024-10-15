5ed53cc20ea056458f22229598f60bf90ecc900c
명훈 <s23065@gsm.hs.kr>
2024-10-16 08:36:30 +0900

    :recycle: :: Changed VERSION_CODE And VERSION_NAME

--- buildSrc/src/main/kotlin/ProjectProperties.kt
+++ buildSrc/src/main/kotlin/ProjectProperties.kt
@@ -21,8 +21,8 @@ object ProjectProperties {
         const val MIN_SDK = 26
         const val TARGET_SDK = 34
         const val JVM_TARGET = "17"
-        const val VERSION_CODE = 1
-        const val VERSION_NAME = "1.0"
+        const val VERSION_CODE = 20
+        const val VERSION_NAME = "1.2.9"
         const val KOTLIN_COMPILER_EXTENSION = "1.4.3"
 
         val JAVA_VERSION = JavaVersion.VERSION_17
