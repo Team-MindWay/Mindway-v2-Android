2537d0f2f0cbadba6d93bda3a909d9820a2c06b4
youloveli <mymhj122@gmail.com>
2024-10-23 09:04:47 +0900

    release 빌드 타입에서 난독화를 활성화 하도록 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile(ProjectProperties.Files.DEFAULT_PROGUARD_FILES),
                 ProjectProperties.Files.PROGUARD_FILES
