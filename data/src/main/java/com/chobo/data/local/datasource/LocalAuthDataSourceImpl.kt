c092e67f12dcedf763ecdb3148369aeb0d80c2d7
youloveli <mymhj122@gmail.com>
2024-10-26 09:43:07 +0900

    log LocalAuthDataSourceImpl 추가

--- data/src/main/java/com/chobo/data/local/datasource/LocalAuthDataSourceImpl.kt
+++ data/src/main/java/com/chobo/data/local/datasource/LocalAuthDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.chobo.data.local.datasource
 
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -20,65 +21,77 @@ class LocalAuthDataSourceImpl @Inject constructor(
     }
 
     override fun getAccessToken(): Flow<String> = dataStore.data.map {
+        Log.d(ACCESS_TOKEN.toString(), it[ACCESS_TOKEN] ?: "")
         it[ACCESS_TOKEN] ?: ""
     }
 
     override suspend fun setAccessToken(accessToken: String) {
         dataStore.edit {
+            Log.d("set $ACCESS_TOKEN", accessToken)
             it[ACCESS_TOKEN] = accessToken
         }
     }
 
     override suspend fun deleteAccessToken() {
         dataStore.edit {
+            Log.d("delete $ACCESS_TOKEN", "dleele")
             it.remove(ACCESS_TOKEN)
         }
     }
 
     override fun getAccessTime(): Flow<String> = dataStore.data.map {
+        Log.d("$ACCESS_TIME", it[ACCESS_TIME] ?: "")
         it[ACCESS_TIME] ?: ""
     }
 
     override suspend fun setAccessTime(accessTime: String) {
         dataStore.edit {
+            Log.d("set $ACCESS_TIME", accessTime)
             it[ACCESS_TIME] = accessTime
         }
     }
 
     override suspend fun deleteAccessTime() {
         dataStore.edit {
+            Log.d("delete $ACCESS_TIME", "dleele")
             it.remove(ACCESS_TIME)
         }
     }
 
     override fun getRefreshToken(): Flow<String> = dataStore.data.map {
+        Log.d(REFRESH_TOKEN.toString(), it[REFRESH_TOKEN] ?: "")
         it[REFRESH_TOKEN] ?: ""
     }
 
     override suspend fun setRefreshToken(refreshToken: String) {
         dataStore.edit {
+            Log.d("set $REFRESH_TOKEN", refreshToken)
             it[REFRESH_TOKEN] = refreshToken
         }
     }
 
     override suspend fun deleteRefreshToken() {
         dataStore.edit {
+            Log.d("delete $REFRESH_TOKEN", "dleele")
             it.remove(REFRESH_TOKEN)
         }
     }
 
     override fun getRefreshTime(): Flow<String> = dataStore.data.map {
+        Log.d(REFRESH_TIME.toString(), it[REFRESH_TIME] ?: "")
         it[REFRESH_TIME] ?: ""
     }
 
     override suspend fun setRefreshTime(refreshTime: String) {
         dataStore.edit {
+            Log.d("set $REFRESH_TIME", refreshTime)
             it[REFRESH_TIME] = refreshTime
         }
     }
 
     override suspend fun deleteRefreshTime() {
         dataStore.edit {
+            Log.d("delete $REFRESH_TIME", "dleele")
             it.remove(REFRESH_TIME)
         }
     }
