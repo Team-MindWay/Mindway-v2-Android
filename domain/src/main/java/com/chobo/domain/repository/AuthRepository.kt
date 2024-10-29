20dfa1b81779023d00273552e209dacca301a0cf
kimjeakwan <s24042@gsm.hs.kr>
2024-10-29 20:33:45 +0900

    :recycle: :: Delete suspend function

--- domain/src/main/java/com/chobo/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/chobo/domain/repository/AuthRepository.kt
@@ -5,10 +5,10 @@ import com.chobo.domain.model.auth.response.GAuthLoginResponseModel
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
-    suspend fun gAuthLogin(body: GAuthLoginRequestModel): Flow<GAuthLoginResponseModel>
+    fun gAuthLogin(body: GAuthLoginRequestModel): Flow<GAuthLoginResponseModel>
     suspend fun gAuthAccess(refreshToken: String): Flow<GAuthLoginResponseModel>
     fun getGAuthAccess(): Flow<String>
     suspend fun saveLoginData(data: GAuthLoginResponseModel)
-    suspend fun logout(): Flow<Unit>
+    fun logout(): Flow<Unit>
     suspend fun deleteLoginData()
 }
